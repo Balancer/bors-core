9b80f20bd69496db4d048333bef35a1644a98f84
Balancer <balancer@balancer.ru>
2008-06-08 22:27:10 +0400

    +++

---
+++ classes/inc/BorsXml.php
@@ -0,0 +1,47 @@
+<?php
+
+class BorsXml
+{
+	private $parser;
+	private $pointer;
+	var $dom = array();
+	
+	function __construct()
+	{
+		$this->pointer = &$this->dom;
+		$this->parser = xml_parser_create();
+		xml_set_object($this->parser, $this);
+		xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false);
+		xml_set_element_handler($this->parser, "tag_open", "tag_close");
+		xml_set_character_data_handler($this->parser, "cdata");
+	}
+
+	function parse($data)
+	{
+		xml_parse($this->parser, $data);
+	}
+
+	function tag_open($parser, $tag, $attributes)
+	{
+		$attributes['_tag'] = $tag;
+		$attributes['_parent'] = &$this->pointer;
+
+		$this->pointer[$tag][] = &$attributes;
+
+		$this->pointer = &$attributes;
+	}
+
+	function cdata($parser, $cdata)
+	{
+		$this->pointer['cdata'] = &$cdata;
+	}
+
+	function tag_close($parser, $tag)
+	{
+		$parent = &$this->pointer['_parent'];
+		unset($this->pointer['_parent']);
+		unset($this->pointer['_tag']);
+
+		$this->pointer = &$parent;
+	}
+}

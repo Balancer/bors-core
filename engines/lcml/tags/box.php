5dfb0e0688c65f04f905bfa54782dd61cac2d869
Balancer <balancer@balancer.ru>
2008-06-24 15:21:48 +0400

    Фикс разметки.

--- engines/lcml/tags/box.php
+++ engines/lcml/tags/box.php
@@ -3,8 +3,10 @@
     {
 		if(!empty($params['description']))
 			return @$params['_align_b']."<dl class=\"box\"><dt>".lcml($params['description'])."</dt><dd>".lcml($txt)."</dd></dl>".@$params['_align_e'];
+		elseif(!empty($params['_align_b']))
+			return $params['_align_b'].lcml($txt).$params['_align_e'];
 		else
-			return @$params['_align_b'].lcml($txt).@$params['_align_e'];
+			return "<div class=\"box\">".lcml($txt)."</div>";
 
 /*		return <<<__EOT__
 <table border="0" width="{$params['width']}" cellPadding="8" cellSpacing="0" align="{$params['align']}">

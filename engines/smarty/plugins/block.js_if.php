6d6a25a124448cf4501c2ea2d00d8fc076e29f36
Balancer <balancer@balancer.ru>
2008-07-07 10:57:34 +0400

    Исправление формата <script>, плюс однажды указанное условие не обязательно теперь повторять.

--- engines/smarty/plugins/block.js_if.php
+++ engines/smarty/plugins/block.js_if.php
@@ -3,17 +3,21 @@
 //	Пример использования:
 //	{js_if cond="top.me_id > 0"}<a href="/users/report/?object={$p->internal_uri()|urlencode}&cat=1">{/js_if}
 //	Сообщить модератору
-//	{js_if cond="top.me_id > 0"}</a>{/js_if}
+//	{js_if}</a>{/js_if}
 
 
 function smarty_block_js_if($params, $content, &$smarty)
 {
 	if($content == NULL) // Открытие формы
 	{
-		base_object::add_template_data('smarty_block_js_if_cond', $params['cond']);
+		if(!empty($params['cond']))
+			base_object::add_template_data('smarty_block_js_if_cond', $params['cond']);
+
 		return;
 	}
 
-	echo "<script>if(".base_object::template_data('smarty_block_js_if_cond')."){document.write(\"".addslashes(str_replace("\n", '\n', $content))."\")}</script>";
+	echo "<script  type=\"text/javascript\"><!--
+if(".base_object::template_data('smarty_block_js_if_cond')."){document.write(\"".addslashes(str_replace("\n", '\n', $content))."\")}
+--></script>";
 	return;
 }

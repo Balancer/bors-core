03fe3b68db8ff5ab1f5990814fdf8034f734fb9e
Balancer <balancer@balancer.ru>
2008-07-11 08:31:36 +0400

    Упрощённое задание цвета.

--- engines/lcml/tags/colors.php
+++ engines/lcml/tags/colors.php
@@ -1,5 +1,13 @@
 <?
-	foreach(split(' ','red green blue yellow gray silver orange white black') as $color)
-		eval("function lp_$color(\$txt)   { return \"<span style=\\\"color: $color;\\\">\".lcml(\$txt).\"</span>\"; }");
+
+foreach(split(' ','red green blue yellow gray silver orange white black') as $color)
+	eval("function lp_$color(\$txt)   { return \"<span style=\\\"color: $color;\\\">\".lcml(\$txt).\"</span>\"; }");
 	
-    function lp_color($txt, $params)  { return "<span style=\"color:".@$params['name'].";\">".lcml($txt)."</span>";}
+function lp_color($txt, $params)
+{
+	if(empty($params['name']))
+		$color = $params['orig'];
+	else
+		$color = $params['name'];
+	return "<span style=\"color:{$color}\">".lcml($txt)."</span>";
+}

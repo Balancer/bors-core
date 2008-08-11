7b7b3033cd1ddb56fefec0437625bc5429e85d51
Balancer <balancer@balancer.ru>
2008-08-11 13:43:11 +0400

    Новый вид ссылок/

---
+++ classes/url/calling2.php
@@ -0,0 +1,19 @@
+<?php
+
+class url_calling2 extends url_base
+{
+	function url($page = NULL)
+	{
+		$obj = $this->id();
+		$url = $obj->called_url();
+		
+		if(preg_match('!^(.+/)\w+\.html$!', $url, $m))
+			return (!$page || $page == $obj->default_page()) ? $m[1] : $m[1].$page.'.html';
+
+		if(preg_match('!^.+/\w+/$!', $url))
+			return (!$page || $page == $obj->default_page()) ? $url : preg_replace('!/$!', "/{$page}.html", $url);
+
+		return $url;
+//		debug_exit("Unknown calling url format: '{$url}' for {$this->id()->class_name()}({$this->id()->id()})");
+	}
+}

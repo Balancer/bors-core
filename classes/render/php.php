b0700e3a053de04574916a7f57f18675996c6f81
Balancer <balancer@balancer.ru>
2008-09-07 23:52:23 +0400

    fix

--- classes/render/php.php
+++ classes/render/php.php
@@ -6,10 +6,14 @@ class render_php extends base_null
 	{
 		if(!$object->loaded() && !$object->can_be_empty())
 			return false;
-			
+
+
 		foreach($object->local_template_data_array() as $var => $value)
 			$$var = &$value;
-		
+
+		foreach($object->local_template_data_set() as $var => $value)
+			$$var = &$value;
+
 		$self = $object;
 		ob_start();
 		require(preg_replace('!\.php$!', '.tpl.php', $object->class_file()));

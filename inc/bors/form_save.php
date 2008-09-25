7345ed302e1fe603d506e644e97f2a0c4413d993
Balancer <balancer@balancer.ru>
2008-09-25 09:06:01 +0400

    fixes

--- inc/bors/form_save.php
+++ inc/bors/form_save.php
@@ -150,8 +150,10 @@ function bors_form_save_object($class_name, $id, &$data, $first, $last)
 		$object->$method($data);
 	}
 
+//	print_d($data);
+
 	if(($file_data = @$data['uploaded_file']) && $file_data['tmp_name'])
-		$object->{'upload_'.$file_data['upload_name'].'_file'}($file_data, $data);
+		$object->{'upload_'.$data['upload_name'].'_file'}($file_data, $data);
 
 //	echo "Set fields for $object: ".print_d($data, true)."<br/>"; set_loglevel(10,0);
 	if($first)

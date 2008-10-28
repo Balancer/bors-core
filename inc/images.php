89fb804c660f08c9e8d8d2502c660771acdbab95
Balancer <balancer@balancer.ru>
2008-10-28 11:57:07 +0300

    Фикс масштабирования картинок.

--- inc/images.php
+++ inc/images.php
@@ -4,30 +4,40 @@ require_once 'Image/Transform.php';
 
 function image_file_scale($file_in, &$file_out, $width, $height, $opts)
 {
+//	echo "image_file_scale($file_in, $file_out, $width, $height, $opts)";
 	$img =& Image_Transform::factory(config('image_transform_engine'));
 	
 	if(PEAR::isError($img))
 		return $img;
 
-	if(!$width)
-		$width = $height * 100 + 64;
-	if(!$height)
-		$height = $width * 100 + 64;
-
 	$img->load($file_in);
 	if(!$opts)
+	{
+		if(!$width)
+			$width = $height * 100 + 64;
+		if(!$height)
+			$height = $width * 100 + 64;
+
 		$img->fit($width, $height);
+	}
 	else
 	{
 		$opts = explode(',', $opts);
 		
 		$img_h = $img->getImageHeight();
 		$img_w = $img->getImageWidth();
+
+		if(!$width)
+			$width = $height * $img_w / $img_h;
+		if(!$height)
+			$height = $width * $img_h / $img_w;
 	
-		$up = in_array('up', $opts);
+		$scale_up = in_array('up', $opts);
 		$crop = in_array('crop', $opts);
 		
-		if($up || ($img_h >= $height && $img_w >= $width)) // ресайз обязателен
+		$scale_down = ($height && $img_h >= $height) || ($width && $img_w >= $width);
+		
+		if($scale_up || $scale_down) // ресайз обязателен
 		{
 			$upw = $img_w*$height/$img_h;
 			if($upw > $width)
@@ -37,11 +47,12 @@ function image_file_scale($file_in, &$file_out, $width, $height, $opts)
 				$upw = $width;
 				$uph = $img_h*$width/$img_w;
 			}
-			
-//			bors_exit("img={$img_w}x{$img_h}, need={$width}x{$height}, up=$up, crop=$crop, upwh={$upw}x{$uph}");
+					
 			$img->resize($upw, $uph);
 			if($upw > $width || $uph > $height)
 				$img->crop($width, $height, ($upw-$width)/2, ($uph-$height)/2);
+
+//			bors_exit("img={$img_w}x{$img_h}, need={$width}x{$height}, up=$scale_up, crop=$crop, upWxH={$upw}x{$uph}");
 		}
 		
 	}

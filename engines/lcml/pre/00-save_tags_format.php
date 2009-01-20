f8afa40f6076d34d3e1b5672c280fb5e0d111b00
Balancer <balancer@balancer.ru>
2009-01-20 19:38:43 +0300

    Исправляем обратные слеши в code-блоках.

--- engines/lcml/pre/00-save_tags_format.php
+++ engines/lcml/pre/00-save_tags_format.php
@@ -5,8 +5,8 @@
     {
 		foreach(explode(' ', 'code music') as $tag)
 		{
-			$txt = preg_replace("!(\[$tag\])(.+?)(\[/$tag\])!ise", "'$1'.save_format(stripslashes('$2')).'$3'", $txt);
-			$txt = preg_replace("!(\[$tag [^\]]+\])(.+?)(\[/$tag\])!ise", "'$1'.save_format(stripslashes('$2')).'$3'", $txt);
+			$txt = preg_replace("!(\[$tag\])(.+?)(\[/$tag\])!ise", "\"$1\".save_format(\"$2\").\"$3\"", $txt);
+			$txt = preg_replace("!(\[$tag [^\]]+\])(.+?)(\[/$tag\])!ise", "\"$1\".save_format(\"$2\").\"$3\"", $txt);
 		}
 		
         return $txt;

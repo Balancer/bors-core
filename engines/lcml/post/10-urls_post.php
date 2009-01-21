a4fa2c3fa17ab3cefec8b53dcba5bdb7c4a8aeed
Balancer <balancer@balancer.ru>
2009-01-21 03:01:22 +0300

    Патчим древний кривой код.

--- engines/lcml/post/10-urls_post.php
+++ engines/lcml/post/10-urls_post.php
@@ -2,6 +2,7 @@
     function lcml_urls_post($txt)
     {
         $txt = preg_replace("!\[url=(.+?)\](.+?)\[/url\]!is", "<a href=\"$1\">$2</a>", $txt);
+        $txt = preg_replace("!\[url=(.+?)\]!i", "<a href=\"$1\">$1</a>", $txt);
         $txt = preg_replace("!\[img=(.+?)\]!is", "<img src=\"$1\" />", $txt);
 
         return $txt;

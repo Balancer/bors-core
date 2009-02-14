33a10467ecd6995e528a491297e226ff06843cd0
Balancer <balancer@balancer.ru>
2009-02-15 01:42:54 +0300

    Перестанем изобретать велосипед в деле маскировки разметки.

--- engines/lcml/funcs.php
+++ engines/lcml/funcs.php
@@ -1,37 +1,30 @@
-<?
-    function c_type($txt)
-    {
-        if(preg_match("/^[0-9]$/",$txt)) return 1;
-        if(preg_match("/^[A-Za-z]$/",$txt)) return 2;
-        if(preg_match("/^[а-яА-Я]$/u",$txt)) return 3;
-        return 0;
-    }
+<?php
 
-    function check_lcml_access($var, $default=false)
-    {
-        return $default;
-    }
+function c_type($txt)
+{
+	if(preg_match("/^[0-9]$/",$txt)) return 1;
+	if(preg_match("/^[A-Za-z]$/",$txt)) return 2;
+	if(preg_match("/^[а-яА-Я]$/u",$txt)) return 3;
+	return 0;
+}
 
-    function save_format($txt)
-    {
-        $txt = str_replace(
-			array(" ","\n","<",">","&", "*","#",'[',']', '$'),
-			array("---save_space---","---save_cr---","---less---","---great---","---ampersand---","---mul---","---hash---",'---square-bracket-open---','---square-bracket-close---', '---dollar-sign---'),
-			$txt);
-        return $txt;
-    }
+function check_lcml_access($var, $default=false)
+{
+	return $default;
+}
 
-    function restore_format($txt)
-    {
-        $txt = str_replace(
-			array("---save_space---","---save_cr---","---less---","---great---","---ampersand---","---mul---","---hash---",'---square-bracket-open---','---square-bracket-close---', '---dollar-sign---'), 
-			array(" ","\n","<",">","&","*","#",'[',']', '$'), 
-			$txt);
-        return $txt;
-    }
+function save_format($txt)
+{
+	return '>>>base64-save-format>>>'.chunk_split(base64_encode($txt), 32, '# #').'<<<base64-save-format<<<';
+}
 
-    function remove_format($txt)
-    {
-        $txt = preg_replace(array("!\s+!","!\n!"),array(" "," "),$txt);
-        return $txt;
-    }
+function restore_format($txt)
+{
+	return preg_replace('/>>>base64-save-format>>>(.+)<<<base64-save-format<<</e', "base64_decode(str_replace('# #', '', '$1'))", $txt);
+}
+
+function remove_format($txt)
+{
+	$txt = preg_replace(array("!\s+!","!\n!"),array(" "," "),$txt);
+	return $txt;
+}

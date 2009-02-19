c22333f5c6391ae21873e662eaeec1dc28b79316
Balancer <balancer@balancer.ru>
2009-02-19 12:31:06 +0300

    Ошибка с обработкой в dropdown уже готовых списков.

--- engines/smarty/plugins/function.dropdown.php
+++ engines/smarty/plugins/function.dropdown.php
@@ -23,30 +23,33 @@ function smarty_function_dropdown($params, &$smarty)
 
 //	echo "==={$list}===";
 
-	if(preg_match("!^(\w+)\->(\w+)$!", $list, $m))
+	if(!is_array($list))
 	{
-		if($m[1] == 'this')
-			$list = $obj->$m[2]();
+		if(preg_match("!^(\w+)\->(\w+)$!", $list, $m))
+		{
+			if($m[1] == 'this')
+				$list = $obj->$m[2]();
+			else
+				$list = object_load($m[1])->$m[2]();
+		}
+		elseif(preg_match("!^(\w+)\->(\w+)\('(.+)'\)!", $list, $m))
+		{
+			if($m[1] == 'this')
+				$list = $obj->$m[2]($m[3]);
+			else
+				$list = object_load($m[1])->$m[2]($m[3]);
+		}
+		elseif(preg_match("!^\w+$!", $list))
+		{
+			$list = &new $list(@$args);
+			$list = $list->named_list();
+		}
 		else
-			$list = object_load($m[1])->$m[2]();
+		{
+			eval('$list='.$list);
+		}
 	}
-	elseif(preg_match("!^(\w+)\->(\w+)\('(.+)'\)!", $list, $m))
-	{
-		if($m[1] == 'this')
-			$list = $obj->$m[2]($m[3]);
-		else
-			$list = object_load($m[1])->$m[2]($m[3]);
-	}
-	elseif(preg_match("!^\w+$!", $list))
-	{
-		$list = &new $list(@$args);
-		$list = $list->named_list();
-	}
-	else
-	{
-		eval('$list='.$list);
-	}
-
+	
 	if(empty($get))
 		$current = preg_match('!^\w+$!', $name) ? (isset($value)?$value:$obj->$name()) : 0;
 	else

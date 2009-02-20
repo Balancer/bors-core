813f26299af16859da6802aa97bfb3db2dc92ac3
Balancer <balancer@balancer.ru>
2009-02-20 16:31:21 +0300

    Дорабатываем модификатор.

--- engines/smarty/plugins/modifier.sklon.php
+++ engines/smarty/plugins/modifier.sklon.php
@@ -1,13 +1,14 @@
 <?php
-function smarty_modifier_sklon($n, $s1, $s2, $s5) // 1 нож 2 ножа 5 ножей
+function smarty_modifier_sklon($n, $s1, $s2=NULL, $s5=NULL) // 1 нож 2 ножа 5 ножей
 {
+	if($s2 === NULL)
+		list($s1, $s2, $s5) = explode(',', $s1);
+			
     $ns=intval(substr($n,-1));
     $n2=intval(substr($n,-2));
 
     if($n2>=10 && $n2<=19) return $s5;
     if($ns==1) return $s1;
     if($ns>=2&&$ns<=4) return $s2;
-    if($ns==0 || $ns>=5) return $s5;
-    die("Неизвестная пара '$n $s1'! Пожалуйста, сообщи об этой ошибке Администратору!");
+    return $s5;
 }
-?>

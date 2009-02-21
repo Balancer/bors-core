dcfc865415f8791fa2772631d84bdb1748444f7d
Balancer <balancer@balancer.ru>
2009-02-21 03:36:21 +0300

    С factory пока никак.

--- classes/bors/base/null.php
+++ classes/bors/base/null.php
@@ -4,14 +4,6 @@ class base_null
 {
 	function can_be_empty() { return true; }
 	function class_name() { return get_class($this); }
-	function get_class_static($skip = 0)
-	{
-		$bt = debug_backtrace();
-		// note that we are using $bt[1] instead of $bt[0];
-		//$bt[0] would return the get_class_static function rather than the calling class
-		$name = $bt[$skip+1]['class'];
-		return $name;
-	}
 	function init() { return false; }
 	function _configure() { return false; }
 	function can_cached() { return false; }

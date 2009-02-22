38b1f88e1c915fd81852ce476437b0433b3aa09e
Balancer <balancer@balancer.ru>
2009-02-23 00:19:14 +0300

    Патчим старый код на форумах + Iceweasel в определении браузеров.

--- engines/lcml/pre/01-old_forums_patches.php
+++ engines/lcml/pre/01-old_forums_patches.php
@@ -6,5 +6,7 @@ function lcml_old_forums_patches($txt)
 	$txt = preg_replace('!<a href=([^>]+)>\\1\[/URL\]!ie', "save_format('<a href=\"$1\">'.stripq('$1').'</a>')", $txt);
 	$txt = preg_replace("!<a href=\"([^\"]+)\">(.+?)\[/URL\]!ise", "save_format('<a href=\"$1\">'.lcml(stripq('$2')).'</a>')", $txt);
 
+	$txt = preg_replace("!<font size=([\-\d]+) color=([#\d]+)>(.*?)</font>!is", "[font $1][size $2]$3[/size][/font]", $txt);
+
 	return $txt;
 }

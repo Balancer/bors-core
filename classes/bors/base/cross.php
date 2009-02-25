abe9435ab63499bb11338d9429c2cee3bc525813
Balancer <balancer@balancer.ru>
2009-02-25 03:53:00 +0300

    up

--- classes/bors/base/cross.php
+++ classes/bors/base/cross.php
@@ -13,22 +13,22 @@ class base_cross extends base_page_db
 		$this->cross_ids = $this->db()->get_array("
 				SELECT `".$this->cross_storage_id_to()."`
 					FROM `".$this->main_table_storage()."`
-					WHERE `".$this->cross_storage_id_from()."` = '".addslashes($this->id)."'");
+					WHERE `".$this->cross_storage_id_from()."` = '".addslashes($this->id())."'");
 
 		return $this->cross_ids;
 	}
 
 	function clean()
 	{
-		$this->db()->query("DELETE FROM `".$this->main_table_storage()."` WHERE `".$this->cross_storage_id_from()."` = '".addslashes($this->id)."'");
+		$this->db()->query("DELETE FROM `".$this->main_table_storage()."` WHERE `".$this->cross_storage_id_from()."` = '".addslashes($this->id())."'");
 	}
 
 	function setup($to_id)
 	{
 		$this->db()->store($this->main_table_storage(), 
-				"`".$this->cross_storage_id_from()."` = '".addslashes($this->id).
+				"`".$this->cross_storage_id_from()."` = '".addslashes($this->id()).
 				"' AND `".$this->cross_storage_id_to()."` = '".addslashes($to_id)."'", array(
-			$this->cross_storage_id_from() => $this->id,
+			$this->cross_storage_id_from() => $this->id(),
 			$this->cross_storage_id_to() => $to_id,
 		));
 	}

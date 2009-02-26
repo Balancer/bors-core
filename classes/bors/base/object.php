4eefb4a93433ed26eb3acab94d033f53bb4d5683
Balancer <balancer@balancer.ru>
2009-02-26 09:43:33 +0300

    Дорабатываем автоматические страницы с разбивкой и редакторы.

--- classes/bors/base/object.php
+++ classes/bors/base/object.php
@@ -358,12 +358,15 @@ class base_object extends base_empty
 		return "<a href=\"{$this->edit_url($this->page())}\"><img src=\"/bors-shared/images/edit-16.png\" width=\"16\" height=\"16\" alt=\"edit\" title=\"$title\"/></a>";
 	}
 
-	function imaged_delete_link($title = NULL, $text = '') { return $this->imaged_delete_url($title, $text); }
+	function imaged_delete_link($text = NULL, $title = NULL) { return $this->imaged_delete_url($title, $text); }
 	
 	function imaged_delete_url($title = NULL, $text = '')
 	{
 		if($title === NULL)
 			$title = ec('Удалить ').strtolower($this->class_title_rp());
+
+		if($text === NULL)
+			$text = $title;
 
 		if($text)
 			$text = '&nbsp;'.$text;

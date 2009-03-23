f5bae3e4b44e5e27db7dd5d67d741c17879ea733
Balancer <balancer@balancer.ru>
2009-03-23 10:26:11 +0300

    ...

--- classes/bors/cache/group.php
+++ classes/bors/cache/group.php
@@ -2,7 +2,7 @@
 
 class cache_group extends base_object_db
 {
-	function main_db_storage() { return config('cache_database'); }
+	function main_db() { return config('cache_database'); }
 	function main_table() { return 'cache_groups'; }
 	function main_table_fields() { return array('id', 'cache_group', '_target_class_id', '_target_object_id', 'create_time'); }
 

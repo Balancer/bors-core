a967244038415e907f03329e9a3d0b2c06efc1b5
Balancer <balancer@balancer.ru>
2009-03-23 13:03:15 +0300

    Фиксы против ужесточения проверок данных.

--- engines/smarty/plugins/function.input.php
+++ engines/smarty/plugins/function.input.php
@@ -6,7 +6,7 @@
 		if(!isset($value))
 		{
 			$obj = $smarty->get_template_vars('current_form_class');
-			$value = preg_match('!^\w+$!', $name) ? (isset($value)?$value:$obj->$name()) : '';
+			$value = preg_match('!^\w+$!', $name) ? (isset($value)?$value : ($obj?$obj->$name():NULL)) : '';
 		}
 			
 		if(!isset($value) && isset($def))

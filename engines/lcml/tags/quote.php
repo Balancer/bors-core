e84d59f0ae62088a3fae96737afdbaad40f93c27
Balancer <balancer@balancer.ru>
2009-04-17 05:43:31 +0400

    merge

--- engines/lcml/tags/quote.php
+++ engines/lcml/tags/quote.php
@@ -1,9 +1,10 @@
-<?
-    function lp_quote($txt, $params)
-    {
-		if(empty($params['description']))
-			$out = " <blockquote>";
-		else
-			$out = " <blockquote><small><b><div class=\"quotetop\" style=\"border-bottom-width: 1px; border-bottom-style: solid;\">{$params['description']}</div></b></small>";
-		return $out.'<div>'.lcml(trim($txt)).'</div></blockquote> ';
-    }
+<?php
+
+function lp_quote($txt, $params)
+{
+	if(empty($params['description']))
+		$out = " <blockquote>";
+	else
+		$out = " <blockquote><small><b><div class=\"quotetop\" style=\"border-bottom-width: 1px; border-bottom-style: solid;\">{$params['description']}</div></b></small>";
+	return $out.'<div>'.lcml(trim($txt)).'</div></blockquote> ';
+}

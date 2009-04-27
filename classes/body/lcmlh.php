36216e750758e979fd419891d60d77b9f2b4c618
Balancer <balancer@balancer.ru>
2009-04-27 07:52:03 +0400

    Начинаем понемногу работать над будущей IDE :)

---
+++ classes/body/lcmlh.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('engines/lcml/main.php');
+
+class body_lcmlh extends base_null
+{
+	function body($obj)
+	{
+		$data = array();
+
+		//TODO: Вычистить все _queries.
+		if($qlist = $obj->_queries())
+		{
+			$db = new DataBase($obj->main_db());
+
+			foreach($qlist as $qname => $q)
+			{
+				$cache = false;
+				if(preg_match("!^(.+)\|(\d+)$!s", $q, $m))
+				{
+					$q		= $m[1];
+					$cache	= $m[2];
+				}
+
+				if(preg_match("/!(.+)$/s", $q, $m))
+					$data[$qname] = $db->get($m[1], false, $cache);
+				else
+					$data[$qname] = $db->get_array($q, false, $cache);
+			}
+		}
+
+		$data['template_dir'] = $obj->class_dir();
+		$data['this'] = $obj;
+
+		$obj->template_data_fill();
+		require_once('engines/smarty/assign.php');
+		return lcml_h(template_assign_data($obj->body_template(), $data));
+	}
+}
---
+++ classes/bors/ext/admin/core/edit.php
@@ -0,0 +1,41 @@
+<?php
+
+class bors_ext_admin_core_edit extends base_page
+{
+	//TODO: добавить access
+	function title() { return ec('Редактор класса ').$this->id(); }
+
+	function body_engine() { return 'body_lcmlh'; }
+	function acl_edit_sections() { return array('*' => 10); }
+
+	function pre_show()
+	{
+		$this->class_path = class_include($this->id());
+		return false;
+	}
+
+	function local_data()
+	{
+		$class_name = $this->id();
+		return array(
+			'class_path' => $this->class_path,
+			'class' => new $class_name(NULL),
+			'title' => $this->get_func_ret('title'),
+			'class_title' => $this->get_func_ret('class_title'),
+			'class_title_rp' => $this->get_func_ret('class_title_rp'),
+			'main_db' => $this->get_func_ret('main_db'),
+			'main_table' => $this->get_func_ret('main_table'),
+		);
+	}
+
+	function target_source() { return file_get_contents($this->class_path); }
+
+	function get_func_ret($func_name)
+	{
+//		print_d($this->target_source());
+		if(preg_match('!function\s+'.preg_quote($func_name).'\(\)\s+\{\s+return\s+([^}]+)\s*;\s*\}!sx', $this->target_source(), $m))
+			return $m[1];
+		
+		return false;
+	}
+}

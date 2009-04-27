f3c746d889a1e77c4340f0f0d5fad582528e89bb
Balancer <balancer@balancer.ru>
2009-04-27 07:52:03 +0400

    Начинаем понемногу работать над будущей IDE :)

--- engines/lcml/main.php
+++ engines/lcml/main.php
@@ -170,6 +170,23 @@ function lcml($text, $params = array())
 	return $res;
 }
 
+function lcml_h($string)
+{
+	$se = config('lcml_tags_enabled');
+	$sd = config('lcml_tags_disabled');
+	config_set('lcml_tags_enabled', NULL);
+	config_set('lcml_tags_disabled', NULL);
+	$result = lcml($string, array(
+			'cr_type' => 'none',
+			'sharp_not_comment' => true,
+			'html_disable' => false,
+			'nocache' => true,
+	));
+	config_set('lcml_tags_enabled', $se);
+	config_set('lcml_tags_disabled', $sd);
+	return $result;
+}
+
 function lcml_bb($string)
 {
 	return lcml($string, array(

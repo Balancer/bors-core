1864bdffa89d84c79ba6ed86bd3ee45a85c0a624
Balancer <balancer@balancer.ru>
2009-05-07 05:51:27 +0400

    Немного причёсываем конфиг. Вводим переменную BORS_SITE взамен неоднозначной BORS_HOST.

--- config/default.php
+++ config/default.php
@@ -2,7 +2,7 @@
 
 // Если не ноль, то боты, при превышении LoadAverage данной величины, получают сообщение о временной недоступности сервиса
 config_set('bot_lavg_limit', 0);
-config_set('cache_dir', $_SERVER['DOCUMENT_ROOT'].'/../.cache');
+config_set('cache_dir', '/tmp/bors-cache');
 
 config_set('debug_class_load_trace', true);
 
@@ -15,11 +15,15 @@ config_set('bors_version_show', '2');
 config_set('lcml_sharp_markup', false);
 config_set('temporary_file_contents', file_get_contents(dirname(__FILE__).'/../resources/temporary.html'));
 
+// Максимальные ширина, высота и площадь картинки для обработки
+// При их преышении ресайз картинки не производится
+// TODO: сделать очередь системной обработки больших картинок
 config_set('images_resize_max_width', 2048);
 config_set('images_resize_max_height', 2048);
+config_set('images_resize_max_area', 5000000);
 
 // Кодировки
-config_set('internal_charset', 'utf-8');
-config_set('db_charset', 'utf-8');
+config_set('internal_charset', 'utf-8');		// Внутренняя кодировка фреймворка, обычно равна системной
+config_set('db_charset', 'utf-8');				// Кодировка БД
 config_set('default_character_set', 'utf-8');
 config_set('locale', 'ru_RU.UTF-8');

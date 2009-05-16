471442067095f8e160a241149b610980cae19a16
Balancer <balancer@balancer.ru>
2009-05-16 12:45:24 +0400

    Объединяем bors-ext и bors-core.

--- handlers/bors_map.php
+++ handlers/bors_map.php
@@ -25,9 +25,16 @@ $map = array(
 	'/admin/edit\-smart/ => bors_admin_edit_smart',
 	'/admin/cross_unlink\?.* => bors_admin_cross_unlink',
 
+	'/_bors/ => bors_admin_main',
 	'/_bors/admin/append/child\?object=([^&]*) => bors_admin_append_child(1)',
 	'/_bors/admin/edit/page\?object=([^&]+) => bors_admin_edit_page(1)',
 	'/_bors/admin/property\?object=([^&]*) => bors_admin_property(1)',
+	'/_bors/users/do\-login\.bas => bors_admin_users_login',
+	'/_bors/users/do\-logout\.bas => bors_admin_users_logout',
+
+//	Заглушки для удобства.
+	'/login/? => bors_admin_users_login',
+	'/\?login => bors_admin_users_login',
 
 	'/___/ => bors_ext_admin_main',
 	'/___/core/ => bors_ext_admin_core_main',

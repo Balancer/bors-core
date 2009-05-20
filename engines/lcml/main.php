8f9880ff0b6d10e8d0b80cc465473fc4cf14cfa8
Balancer <balancer@balancer.ru>
2009-05-21 03:34:07 +0400

    Возможность скипнуть загрузку классов.

--- engines/lcml/main.php
+++ engines/lcml/main.php
@@ -215,3 +215,20 @@ function lcml_bbh($string)
 	config_set('lcml_tags_disabled', $sd);
 	return $result;
 }
+
+function lcml_smart($string)
+{
+	$se = config('lcml_tags_enabled');
+	$sd = config('lcml_tags_disabled');
+	config_set('lcml_tags_enabled', NULL);
+	config_set('lcml_tags_disabled', NULL);
+	$result = lcml($string, array(
+			'cr_type' => 'smart',
+			'sharp_not_comment' => true,
+			'html_disable' => false,
+			'nocache' => true,
+	));
+	config_set('lcml_tags_enabled', $se);
+	config_set('lcml_tags_disabled', $sd);
+	return $result;
+}

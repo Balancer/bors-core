95bffbd569bf1df8fe77b84332c0c8cb26627c6d
Balancer <balancer@balancer.ru>
2009-06-23 03:28:36 +0400

    Упущенный файлик.

--- engines/lcml/post/00-heads.php
+++ engines/lcml/post/00-heads.php
@@ -1,13 +1,15 @@
-<?
-    function lcml_heads($txt)
-    { 
+<?php
+function lcml_heads($txt)
+{ 
+	$txt = preg_replace("/=== cut ===/","<table class=\"null w100p\"><tr><td width=\"20\"><hr/></td><td width=\"10\">✂</td><td><hr/></td></tr></table>", $txt);
+
 	if(config('lcml_old_exclamation_heads'))
 	{
-            $txt=preg_replace("/^!!!!!(.+)$/me","'<h6>'.lcml(\"$1\").'</h6>'",$txt);
+		$txt=preg_replace("/^!!!!!(.+)$/me","'<h6>'.lcml(\"$1\").'</h6>'",$txt);
 	    $txt=preg_replace("/^!!!!(.+)$/me","'<h5>'.lcml(\"$1\").'</h5>'",$txt);
-            $txt=preg_replace("/^!!!(.+)$/me","'<h4>'.lcml(\"$1\").'</h4>'",$txt);
+        $txt=preg_replace("/^!!!(.+)$/me","'<h4>'.lcml(\"$1\").'</h4>'",$txt);
 	    $txt=preg_replace("/^!!(.+)$/me","'<h3>'.lcml(\"$1\").'</h3>'",$txt);
-            $txt=preg_replace("/^!(.+)$/me","'<h2>'.lcml(\"$1\").'</h2>'",$txt);
+		$txt=preg_replace("/^!(.+)$/me","'<h2>'.lcml(\"$1\").'</h2>'",$txt);
 	}
 	
         $txt=preg_replace("/^ *===== (.+) ===== *$/me","'<h6>'.lcml(\"$1\").'</h6>'",$txt);
@@ -20,4 +22,4 @@
         $txt=preg_replace("/^\s*= (.+) =\s*$/me","'<h2>'.lcml(\"$1\").'</h2>'",$txt);
 
         return $txt;
-    }
+}

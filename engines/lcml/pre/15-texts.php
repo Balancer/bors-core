0b505d55d90081871209d96e978e58a1fa7ba193
Balancer <balancer@balancer.ru>
2009-08-25 12:45:51 +0400

    fixes

--- engines/lcml/pre/15-texts.php
+++ engines/lcml/pre/15-texts.php
@@ -3,5 +3,15 @@
 function lcml_texts($text)
 {
 	$text = preg_replace('!^\-{3,}!m', '[hr]', $text);
+
+	$text = preg_replace("/=== cut ===/","<table class=\"null w100p\"><tr><td width=\"20\"><hr/></td><td width=\"10\">✂</td><td><hr/></td></tr></table>", $text);
+
+	for($i=5; $i>=1; $i--)
+	{
+		$pad = str_repeat('=', $i);
+		$ih = $i + 1;
+		eval("\$text = preg_replace(\"/^ *{$pad} (.+) {$pad} *\$/me\", \"'[h{$ih}]'.lcml(stripq('\$1')).'[/h{$ih}]'\", \$text);");
+	}
+
 	return $text;
 }

6b45c5d6546c42f8fef9ad0754c76c6e454ff24b
Balancer <balancer@balancer.ru>
2009-09-15 00:17:01 +0400

    Нафиг отладочную инфу.

--- classes/bors/base/page.php
+++ classes/bors/base/page.php
@@ -170,7 +170,6 @@ class base_page extends base_object
 
 		$this->template_data_fill();
 		require_once('engines/smarty/assign.php');
-		debug_hidden_log('assign', "assign to ".$this->body_template()."<br/>");
 		return template_assign_data($this->body_template(), $data);
 	}
 

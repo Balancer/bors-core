645269ab2b0de061a2a3e3a3dd1efa50803818d9
Balancer <balancer@balancer.ru>
2009-09-20 03:21:58 +0400

    ++

---
+++ tools/sql-updates/200908131335.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `bors_pictures_thumbs` ADD INDEX ( `relative_path` )
+ALTER TABLE `bors_pictures_thumbs` ADD INDEX ( `file_name` )
+ALTER TABLE `bors_pictures_thumbs` ADD INDEX ( `width` )
+ALTER TABLE `bors_pictures_thumbs` ADD INDEX ( `height` )

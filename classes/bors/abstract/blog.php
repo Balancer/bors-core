7514abd75c03e5490fe3e93dbb7337b8389dcff9
Balancer <balancer@balancer.ru>
2009-09-24 03:38:35 +0400

    Абстрактные блоги.

---
+++ classes/bors/abstract/blog.php
@@ -0,0 +1,10 @@
+<?php
+
+class bors_abstract_blog extends base_page_paged
+{
+	function title() { return ec('Блоги'); }
+	function nav_name() { return ec('блоги'); }
+
+	function where() { return array('is_public' => 1); }
+	function is_reversed() { return true; }
+}

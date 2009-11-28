e52067c9504ba7ac6962d230a8f9e80417d34400
Balancer <balancer@balancer.ru>
2009-11-29 00:35:40 +0300

    ++

--- inc/locales.php
+++ inc/locales.php
@@ -30,7 +30,7 @@ function dc($txt, $charset_from = NULL, $charset_to = NULL)
 		return $txt;
 
 	if($charset_to == 'koi8-r' || $charset_to == 'cp866')
-		$txt = str_replace(array('«','»','©'), array('&laquo;','&raquo;', '&copy;'), $txt);
+		$txt = str_replace(array('«','»','©', '–'), array('&laquo;','&raquo;', '&copy;', '&mdash;'), $txt);
 
 	return iconv($charset_from, $charset_to.'//IGNORE', $txt);
 }

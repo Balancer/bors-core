4c3676130cb45993422b6f1a282dd8c597f98f20
Balancer <balancer@balancer.ru>
2009-11-29 00:52:05 +0300

    fix переводов строк

--- engines/lcml/post/00-heads.php
+++ engines/lcml/post/00-heads.php
@@ -3,11 +3,11 @@ function lcml_heads($txt)
 { 
 	if(config('lcml_old_exclamation_heads'))
 	{
-		$txt=preg_replace("/^!!!!!(.+)$/me","'<h6>'.lcml(\"$1\").'</h6>'",$txt);
-	    $txt=preg_replace("/^!!!!(.+)$/me","'<h5>'.lcml(\"$1\").'</h5>'",$txt);
-        $txt=preg_replace("/^!!!(.+)$/me","'<h4>'.lcml(\"$1\").'</h4>'",$txt);
-	    $txt=preg_replace("/^!!(.+)$/me","'<h3>'.lcml(\"$1\").'</h3>'",$txt);
-		$txt=preg_replace("/^!(.+)$/me","'<h2>'.lcml(\"$1\").'</h2>'",$txt);
+		$txt=preg_replace("/^!!!!!(.+)$/me","'<h6>'.lcml(\"$1\").'</h6> '",$txt);
+	    $txt=preg_replace("/^!!!!(.+)$/me","'<h5>'.lcml(\"$1\").'</h5> '",$txt);
+        $txt=preg_replace("/^!!!(.+)$/me","'<h4>'.lcml(\"$1\").'</h4> '",$txt);
+	    $txt=preg_replace("/^!!(.+)$/me","'<h3>'.lcml(\"$1\").'</h3> '",$txt);
+		$txt=preg_replace("/^!(.+)$/me","'<h2>'.lcml(\"$1\").'</h2> '",$txt);
 	}
 	
 //		echo "<xmp>"; echo $txt; echo "</xmp>"; exit();

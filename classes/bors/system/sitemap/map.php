94bf53a127dce2b87f4c091493103755f892aa1d
Balancer <balancer@balancer.ru>
2009-11-29 03:18:58 +0300

    Дорабатываем.

--- classes/bors/system/sitemap/map.php
+++ classes/bors/system/sitemap/map.php
@@ -13,12 +13,28 @@ class bors_system_sitemap_map extends bors_page
 ';
 		foreach(call_user_func(array($class_name, 'sitemap_index'), $_SERVER['HTTP_HOST'], $page, 1000) as $x)
 		{
+			$time = $x->modify_time();
+			$now = time();
+
+				if($now - $time < 7200)
+					$freq = 'always';
+				elseif($now - $time < 86400)
+					$freq = 'hourly';
+				elseif($now - $time < 86400*7)
+					$freq = 'daily';
+				elseif($now - $time < 86400*30)
+					$freq = 'weekly';
+				else
+					$freq = 'montly';
+
 			for($p=1, $total = $x->total_pages(); $p<=$total; $p++)
 			{
+
+
 				echo "	<url>
 		<loc>".$x->url($p)."</loc>
-		<lastmod>".date('c', $x->modify_time())."</lastmod>
-		<changefreq>".($p<$total ? 'yearly' : 'always')."</changefreq>
+		<lastmod>".date('c', $time)."</lastmod>
+		<changefreq>{$freq}</changefreq>
 	</url>
 ";
 			}

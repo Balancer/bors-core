013b753583f836b00e774851d85f2d0c220f1143
Balancer <balancer@balancer.ru>
2010-06-07 01:56:13 +0400

    +

--- inc/strings.php
+++ inc/strings.php
@@ -9,15 +9,15 @@ function sklon($n, $s1, $s2=NULL, $s5=NULL) // 1 нож 2 ножа 5 ножей
 {
 	if($s2 === NULL)
 		list($s1, $s2, $s5) = explode(',', $s1);
-			
+
    	$ns=intval(substr($n,-1));
 		$n2=intval(substr($n,-2));
 
     if($n2>=10 && $n2<=19) return $s5;
    	if($ns==1) return $s1;
     if($ns>=2&&$ns<=4) return $s2;
-   	if($ns==0 || $ns>=5) return $s5;
-	return "Неизвестная пара '$n $s1'! Пожалуйста, сообщи об этой ошибке администратору!";
+
+	return $s5;
 }
 
 function truncate($string, $length = 80, $etc = '...', $break_words = false, $middle = false)

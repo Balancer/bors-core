02216a9c7f8160f7eae3f0221ca148589933a1d2
Balancer <balancer@balancer.ru>
2010-06-17 14:44:43 +0400

    merge

---
+++ tools/xml2array-dump.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once('config.php');
+include_once(BORS_CORE.'/config.php');
+
+$file = $_SERVER['argv'][1];
+xml_dump($file);
+
+
+function xml_dump($file)
+{
+	require_once('classes/inc/BorsXml.php');
+	$xml = new BorsXml;
+	$xml->parse(file_get_contents($file));
+
+	print_d($xml->dom);
+}

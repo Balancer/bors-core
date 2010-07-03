a8c837c9ac7c2a4d216c1da146dc265d35a7f11f
Balancer <balancer@balancer.ru>
2010-07-03 11:37:27 +0400

    ++

---
+++ classes/bors/tools/ajax/validate.php
@@ -0,0 +1,43 @@
+<?php
+
+class bors_tools_ajax_validate extends base_page
+{
+	function pre_show()
+	{
+		debug_hidden_log('test', print_r($_POST, true));
+		/* RECEIVE VALUE */
+		$validateValue = $_POST['validateValue'];
+		$validateId = $_POST['validateId'];
+		$validateError = $_POST['validateError'];
+
+		/* RETURN VALUE */
+		$arrayToJs = array();
+		$arrayToJs[0] = $validateId;
+		$arrayToJs[1] = $validateError;
+
+		switch($validateError)
+		{
+			case 'loginFree':
+				if(objects_first('aviaport_user', array('login' => $validateValue)))
+					$arrayToJs[2] = "false";
+				else
+					$arrayToJs[2] = "true";
+				break;
+
+			case 'emailNotRegistered':
+				if(objects_first('aviaport_user', array('email' => $validateValue)))
+					$arrayToJs[2] = "false";
+				else
+					$arrayToJs[2] = "true";
+				break;
+
+			default:
+				$arrayToJs[2] = "false";
+				break;
+		}
+
+		echo '{"jsonValidateReturn":'.json_encode($arrayToJs).'}';
+
+		return true;
+	}
+}

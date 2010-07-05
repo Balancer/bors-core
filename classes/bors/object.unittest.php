aeb92a1638c60daff2acdf786922250e78151191
Balancer <balancer@balancer.ru>
2010-07-05 06:53:16 +0400

    Ещё немного тестов

--- classes/bors/object.unittest.php
+++ classes/bors/object.unittest.php
@@ -15,5 +15,24 @@ class bors_object_unittest extends PHPUnit_Framework_TestCase
 
 		// Истинный заголовок не определён
         $this->assertNull($object->title_true());
+
+		// Проверяем полную работу base_empty.
+		$object->set_attr('attr1', 'qwe');
+		$object->set_attr('attr2', 'asd');
+		$object->set('set3', 'zxc', false);
+		$object->set_set4('rty', false);
+
+		// Теперь смотрим, как оно сохранилось
+        $this->assertEquals('qwe', $object->attr('attr1'));
+        $this->assertEquals('asd', $object->attr2());
+        $this->assertEquals('zxc', $object->set3());
+        $this->assertEquals('rty', $object->get('set4'));
+        $this->assertEquals('rty', $object->get_data('set4'));
+
+		// И чтобы не запоминалось
+        $this->assertNull($object->get('set5'));
+        $this->assertEquals('uio', $object->get_data('set5', 'uio'));
+        $this->assertNull($object->get('set5'));
+        $this->assertNull($object->get_data('set5'));
     }
 }

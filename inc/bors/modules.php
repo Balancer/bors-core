b623360b37ffeff4ddad57215de31ef4ac677977
Balancer <balancer@balancer.ru>
2010-07-09 08:57:14 +0400

    12-й тикет на АП и расширения.

--- inc/bors/modules.php
+++ inc/bors/modules.php
@@ -1,10 +1,16 @@
 <?php
 
-function bors_tpl_module($class_name, $args = array())
+function bors_tpl_module($class_name, $args = array()) { return bors_tpl_module_ex($class_name, $args); }
+
+function bors_tpl_module_ex($class_name, $args)
 {
+//	var_dump($args);
 	$args['no_load_cache'] = true;
-	$obj = object_load('module_'.$class_name, NULL, $args);
-		
+	$obj = object_load($class_name, NULL, $args);
+
+	if(!$obj)
+		$obj = object_load('module_'.$class_name, NULL, $args);
+
 	if(!$obj)
 		return "Can't load module 'module_{$class_name}'";
 

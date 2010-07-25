c0d6ad10f7f61ef0335b7afe7a33aa18d279c932
Balancer <balancer@balancer.ru>
2010-07-25 08:59:20 +0400

    Восстанавливаем учёт рефереров.

---
+++ classes/bors/external/referer.php
@@ -0,0 +1,103 @@
+<?php
+
+class bors_external_referer
+{
+	static function register($target_url, $referer, $object = false)
+	{
+		if(!$target_url || !$referer)
+			return;
+
+		if($object === false) // если на входе NULL - то уже была неудачная попытка загрузить у вызывающего, так что пропускаем
+			$object = object_load($target_url);
+
+		$norm_referer = self::normalize($referer);
+
+		if(preg_match('!(/translate_c\?hl=|search\?q=cache:|translate\.google\.com|cgi\-bin/readmsg\?id|/translate\?hl)!', $referer))
+			return;
+
+		if(preg_match('!^https?://([^/]+)!', $referer, $m))
+			$ref_domain = $m[1];
+		else
+		{
+			debug_hidden_log('log-parser-error', "Unknown referer {$referer}", false);
+			return;
+		}
+
+		$url_data = parse_url($target_url);
+		$host = $url_data['host'];
+
+		$skip_ref_domains = config('ref_count_skip_domains', array());
+		$skip_target_domains = config('ref_count_skip_target_domains', array());
+
+		if(in_array($host, $skip_target_domains))
+			return;
+
+		if(in_array($ref_domain, $skip_ref_domains))
+			return;
+
+		if($re = config('ref_count_skip_target_regexp'))
+			if(preg_match($re, $target_url))
+				return;
+
+		echo "u=$target_url, ref=$referer\n";
+		print_d($url_data);
+
+		$data = array(
+			'target_class_name' => object_property($object, 'class_name'),
+			'target_object_id' => object_property($object, 'id'),
+			'target_page' => object_property($object, 'page'),
+		);
+
+		if($q = bors_external_search::query_extract($referer))
+		{
+			// Это вход из поисковой системы
+			$data['query'] = $q;
+
+			$search = objects_first('bors_referer_search', $data);
+			if(!$search)
+			{
+				$search = object_new_instance('bors_referer_search', $data);
+				$search->set_create_time(time(), true);
+				$ref_data = parse_url($referer);
+				$search->set_search_engine($ref_data['host'], true);
+				$search->set_count(0, true);
+			}
+
+			$search->set_modify_time(time(), true);
+			$search->set_target_url($target_url, true);
+			$search->set_search_url($referer, true);
+
+			$search->set_count($search->count()+1, true);
+			$search->store();
+		}
+		else
+		{
+			// Это переход по ссылке
+
+			$data['referer_normalized_url'] = $norm_referer;
+
+			$ref_obj = objects_first('bors_referer_link', $data);
+			if(!$ref_obj)
+			{
+				$ref_obj = object_new_instance('bors_referer_link', $data);
+				$ref_obj->set_create_time(time(), true);
+				$ref_obj->set_count(0, true);
+			}
+
+			$ref_obj->set_modify_time(time(), true);
+			$ref_obj->set_target_url($target_url, true);
+			$ref_obj->set_referer_original_url($referer, true);
+
+			$ref_obj->set_count($ref_obj->count()+1, true);
+			$ref_obj->store();
+		}
+	}
+
+	static function normalize($url)
+	{
+		$url = preg_replace('!http://(www|win)\.!', 'http://', $url);
+		$url = preg_replace('!\?PHPSESSID=[0-9a-f]+&!', '?', $url);
+		$url = preg_replace('!#\w+$!', '', $url);
+		return $url;
+	}
+}
---
+++ classes/bors/external/search.php
@@ -0,0 +1,84 @@
+<?php
+
+class bors_external_search
+{
+	static function query_extract($url)
+	{
+//		if(pre)
+
+		static $google_img = array('!&prev=/images%3Fq%3D(.+?)%26!', '$q = urldecode(urldecode($q[1]));');
+		static $w1251_search = '$q = iconv("windows-1251", "utf-8", urldecode($q[1]));';
+
+		$search_engine_domains = array(
+			'go.mail.ru' => array('/q=([^&]+)/', $w1251_search),
+			'google.co.il' => 'q',
+			'google.com.pe' => 'q',
+			'google.com.ua' => 'q',
+			'google.ee' => 'q',
+			'google.it' => 'q',
+			'google.pt' => 'q',
+			'images.yandex.ru' => 'text',
+			'images.google.cl' => $google_img,
+			'images.google.cn' => $google_img,
+			'images.google.co.uk' => $google_img,
+			'images.google.com' => $google_img,
+			'images.google.com.by' => $google_img,
+			'images.google.com.jp' => $google_img,
+			'images.google.com.ua' => $google_img,
+			'images.google.dk' => $google_img,
+			'images.google.fr' => $google_img,
+			'images.google.kz' => $google_img,
+			'images.google.hu' => $google_img,
+			'images.google.ru' => $google_img,
+			'images.google.se' => $google_img,
+			'm.yandex.ru' => 'query',
+			'nigma.ru' => 's',
+			'nova.rambler.ru' => 'query',
+			'search.conduit.com' => 'q',
+			'search.icq.com' => 'q',
+			'search.live.com' => 'q',
+			'search.qip.ru' => 'query',
+			'search.ukr.net' => 'search_query',
+			'us.yhs.search.yahoo.com' => 'p',
+			'www.google.az' => 'q',
+			'www.google.ca' => 'q',
+			'www.google.com' => 'q',
+			'www.google.com.br' => 'q',
+			'www.google.com.by' => 'q',
+			'www.google.com.ua' => 'q',
+			'www.google.com.uk' => 'q',
+			'www.google.de' => 'q',
+			'www.google.gr' => 'q',
+			'www.google.lv' => 'q',
+			'www.google.md' => 'q',
+			'www.google.pl' => 'q',
+			'www.google.ru' => 'q',
+			'www.yandex.ru' => array('/text=([^&]+)/', $w1251_search),
+			'yandex.ru' => 'text',
+		);
+
+		$data = parse_url($url);
+
+		if(!($se = @$search_engine_domains[$data['host']]))
+			return false;
+
+		if(is_array($se))
+		{
+			$re = $se[0];
+			$func = $se[1];
+		}
+		else
+		{
+			$re = "/(\?|&){$se}=([^&]+)/";
+			$func = '$q = urldecode($q[2]);';
+		}
+
+		if(!preg_match($re, $url, $q))
+			return false;
+
+		eval($func);
+//		echo "\t\tSearch $ref_domain: '$q' -> $uri".($obj ? " [{$obj}({$obj->page()})]":'')."\n";
+
+		return $q;
+	}
+}

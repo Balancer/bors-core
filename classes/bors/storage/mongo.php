a893be5363642de56bfb3dba60d6390c4cf2645d
Balancer <balancer@balancer.ru>
2010-07-26 01:49:51 +0400

    Хм. Вроде бы, с memcached починилось.

--- classes/bors/storage/mongo.php
+++ classes/bors/storage/mongo.php
@@ -4,6 +4,12 @@ class bors_storage_mongo extends bors_storage
 {
 	function load($object)
 	{
+		$m = new Mongo();
+		$db = $m->selectDB('BORS');
+		$c = $db->selectCollection('objects');
+		$object->data = $c->findOne(array('_id' => $object->internal_uri_ascii()));
+
+		return $object;
 	}
 
 	function save($object)
@@ -12,9 +18,10 @@ class bors_storage_mongo extends bors_storage
 		$db = $m->selectDB('BORS');
 		$c = $db->createCollection('objects');
 		$c->insert(array_merge(array(
-				'class_name' => $object->class_name(),
-				'object_id' => $object->id()
-			), $object->data));
+			'_id' => $object->internal_uri_ascii(),
+			'class_name' => $object->class_name(),
+			'object_id' => $object->id()
+		), $object->data));
 	}
 
 	function findOne($class_name, $where = array())

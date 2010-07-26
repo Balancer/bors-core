610d97060780bca16bff2e164dbc5411f64a6a18
Balancer <balancer@balancer.ru>
2010-07-26 14:14:24 +0400

    Дорабатываем импорт RSS.

--- classes/bors/external/feeds/entry.php
+++ classes/bors/external/feeds/entry.php
@@ -51,8 +51,9 @@ class bors_external_feeds_entry extends base_object_db
 			$text[] = "\n// Транслировано с [url]{$link}[/url]\n";
 
 		$text = str_replace("/\n{3,}/", "\n\n", join("\n", $text));
-//		if($this->feed()->post_as_topics())
-//		return "[b]{$this->title()}[/b]\n\n$text";
+		if($this->feed()->titles_in_posts())
+			$text = "[b]{$this->title()}[/b]\n\n$text";
+
 		return $text;
 	}
 
@@ -60,25 +61,97 @@ class bors_external_feeds_entry extends base_object_db
 	{
 		$feed = $this->feed();
 
-		if($feed->target_topic_id())
+		$keywords = join(',', $this->full_keywords_list());
+
+		$owner_id = $feed->owner_id();
+		$owner_name = $this->author_name() ? $this->author_name() : $feed->owner_name();
+
+		if($this->target_object_id()) // Уже было запощено ранее.
 		{
-			$topic_id = $feed->target_topic_id();
-			$topic = object_load('balancer_board_topic', $topic_id);
-			$forum_id = $topic->forum_id();
-		}
-		else
-		{
-			$forum_id = $feed->target_forum_id();
-			if(!$forum_id)
-			{
-				$forum_id = common_keyword::best_forum($this->keywords_string());
-				$forum = object_load('balancer_board_forum', $forum_id);
-				echo "Best forum for '{$this->keywords_string()}' is {$forum->debug_title()}\n";
-//				bors_exit();
-			}
+			$post = $this->target();
+			$post->set_owner_id($owner_id, true);
+			$post->set_author_name($owner_name, true);
+			$post->set_source($this->make_source(), true);
+			$post->set_source_html(NULL, true);
+			$post->set_create_time($this->pub_date(), true);
+
+			$post->topic()->recalculate();
+
+			$blog = object_load('balancer_board_blog', $post->id());
+			$blog->set_blogged_time($this->pub_date(), true);
+
+			return;
 		}
 
-		$tags = explode(',', $this->keywords_string() .',' . $feed->append_keywords());
+		// Не были запощены. Ищем топик и форум.
+
+		$topic = $this->find_topic();
+		$forum = $topic->forum();
+
+		$post = object_new_instance('balancer_board_post', array(
+			'topic_id' => $topic->id(),
+			'owner_id' => $owner_id,
+			'author_name' => $owner_name,
+			'source' => $this->make_source(),
+			'create_time' => $this->pub_date(),
+		));
+
+		$topic->recalculate();
+
+		echo "Was created post {$post->debug_title()}\n";
+
+		$forum->update_num_topics();
+
+		$blog = object_new_instance('balancer_board_blog', array(
+			'id' => $post->id(),
+			'keywords_string' => $keywords,
+			'owner_id' => $owner_id,
+			'topic_id' => $topic->id(),
+			'forum_id' => $topic->forum_id(),
+			'blogged_time' => $this->pub_date(),
+			'is_public' => true,
+		));
+
+		echo "Was created blog {$blog->debug_title()}\n";
+
+		$this->set_target_class_name($post->class_name(), true);
+		$this->set_target_object_id($post->id(), true);
+	}
+
+	function find_topic()
+	{
+		$feed = $this->feed();
+
+		if($feed->target_topic_id()) // Если прописан топик
+		{
+			if(!$feed->topics_auto_search()) // и не прописан автопоиск, то это у нас - фиксированный топик:
+			{
+				$topic_id = $feed->target_topic_id();
+				$topic = object_load('balancer_board_topic', $topic_id);
+				return $topic;
+			}
+
+			// В противном случае мы должны искать подходящий топик по ключевым словам.
+			$kws = join(', ', $this->full_keywords_list());
+			$topic_id = common_keyword::best_topic($kws, $feed->target_topic_id());
+			$topic = object_load('balancer_board_topic', $topic_id);
+			echo 'Found topic [def='.$feed->target_topic_id().'] for '.$kws." = {$topic->debug_title()}\n";
+			return $topic;
+//			bors_exit("\nend: $topic_id, {$topic->debug_title()}, {$topic->url()}\n");
+		}
+
+		// Топик не прописан. Ищем дальше
+
+		if(!$forum_id = $feed->target_forum_id()) // Форум не указан. Ищем лучший.
+			$forum_id = common_keyword::best_forum($this->keywords_string());
+
+		return $this->make_topic($forum_id);
+	}
+
+	function full_keywords_list()
+	{
+		$tags = explode(',', $this->keywords_string() .',' . $this->feed()->append_keywords());
+
 		sort($tags);
 		$ftags = array();
 		foreach($tags as $t)
@@ -88,86 +161,27 @@ class bors_external_feeds_entry extends base_object_db
 		$tags = array_values($ftags);
 		sort($tags);
 
-		$keywords = join(', ', $tags);
+		return $tags;
+	}
 
-		$forum = object_load('balancer_board_forum', $forum_id);
+	function make_topic($forum_id)
+	{
+		$owner_id = $this->feed()->owner_id();
+		$owner_name = $this->author_name() ? $this->author_name() : $this->feed()->owner_name();
 
-		$owner_id = $feed->owner_id();
-		$owner_name = $this->author_name() ? $this->author_name() : $feed->owner_name();
+		$topic = object_new_instance('balancer_board_topic', array(
+			'forum_id' => $forum_id,
+			'title'	=> $this->title() ? $this->title() : ec('Без названия'),
+			'is_public' => true,
+			'owner_id'=> $owner_id,
+			'last_poster_name' => $owner_name,
+			'author_name' => $owner_name,
+			'keywords_string' => join(',', $this->full_keywords_list()),
+			'create_time' => $this->pub_date(),
+		));
 
-		echo "update entry {$this->title()} [{$this->keywords_string()}]\n";
-				$forum->update_num_topics();
-		if($this->target_object_id())
-		{
-			$target = $this->target();
-			$target->set_owner_id($owner_id, true);
-			$target->set_author_name($owner_name, true);
-			$target->set_source($this->make_source(), true);
-			$target->set_source_html(NULL, true);
-			$target->set_create_time($this->pub_date(), true);
+		echo "Was created topic {$topic->debug_title()}\n";
 
-			if(!$feed->target_topic_id())
-			{
-				$topic = $target->topic();
-				$topic->set_title($this->title(), true);
-				$topic->set_owner_id($owner_id, true);
-				$topic->set_author_name($owner_name, true);
-				$topic->recalculate();
-			}
-
-			echo "target = ".$target->debug_title()." ({$target->url()})\n";
-
-			$blog = object_load('balancer_board_blog', $target->id());
-			$blog->set_blogged_time($this->pub_date(), true);
-		}
-		else
-		{
-//			echo "feed->post_as_topics = ".$feed->post_as_topics()."\n";
-			if($feed->post_as_topics())
-			{
-				$topic = object_new_instance('balancer_board_topic', array(
-					'forum_id' => $forum_id,
-					'title'	=> $this->title() ? $this->title() : ec('Без названия'),
-					'is_public' => true,
-					'owner_id'=> $owner_id,
-					'last_poster_name' => $owner_name,
-					'author_name' => $owner_name,
-					'keywords_string' => $keywords,
-					'create_time' => $this->pub_date(),
-				));
-
-				$forum->update_num_topics();
-			}
-			else
-				$topic = object_load('balancer_board_topic', $topic_id);
-
-//			echo "topic = ".$topic->debug_title()."\n";
-
-			$post = object_new_instance('balancer_board_post', array(
-				'topic_id' => $topic->id(),
-				'owner_id' => $owner_id,
-				'author_name' => $owner_name,
-				'source' => $this->make_source(),
-				'create_time' => $this->pub_date(),
-			));
-
-			echo "post = ".$post->debug_title()." ({$post->url()})\n";
-
-			$topic->recalculate();
-
-			$blog = object_new_instance('balancer_board_blog', array(
-				'id' => $post->id(),
-				'keywords_string' => $keywords,
-				'owner_id' => $owner_id,
-				'forum_id' => $topic->forum_id(),
-				'blogged_time' => $this->pub_date(),
-				'is_public' => true,
-			));
-
-			echo "blog = ".$blog->debug_title()."\n";
-
-			$this->set_target_class_name($post->class_name(), true);
-			$this->set_target_object_id($post->id(), true);
-		}
+		return $topic;
 	}
 }

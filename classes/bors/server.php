698611b12ccd2ed76ab8f88805215ce31e52d165
Balancer <balancer@balancer.ru>
2010-08-02 02:19:51 +0400

    Играем с обработкой POST-редиректов.

--- classes/bors/server.php
+++ classes/bors/server.php
@@ -5,4 +5,9 @@ class bors_server extends base_object
 	function can_cached() { return true; }
 
 	static function is_utf8() { return config('internal_charset') == 'utf-8'; }
+
+	static function post_redirector_url($url)
+	{
+		return "http://{$_SERVER['HTTP_HOST']}/bors-post-redirector.php?go=".urlencode($url);
+	}
 }

1aa26bc07af87595f94baf7b2cdb689918918714
Balancer <balancer@balancer.ru>
2010-10-06 08:24:24 +0400

    Мелочи.

--- classes/bors/admin/edit.php
+++ classes/bors/admin/edit.php
@@ -1,22 +1,22 @@
 <?php
 
-class bors_admin_edit extends base_page
+class bors_admin_edit extends bors_page
 {
 	function config_class() { return config('admin_config_class'); }
 
 	function title()
 	{
 		if(!$this->id())
-			return ec('Добавить ') . bors_lower($this->object()->class_title_vp());
+			return ec('Добавить ') . bors_lower($this->admin_object()->class_title_vp());
 
-		return ec('Редактируется ') . bors_lower($this->object()->class_title()) . ' ' . $this->object()->title();
+		return ec('Редактируется ') . bors_lower($this->admin_object()->class_title()) . ec(' «') . $this->real_object()->title().ec('»');
 	}
 
 	function nav_name()
 	{
 		return $this->id() ? ec('редактор') : ec('добавить');
 	}
-
+/*
 	private $_object = false;
 	function object()
 	{
@@ -25,6 +25,16 @@ class bors_admin_edit extends base_page
 
 		return $this->_object = object_load($this->main_class(), $this->id());
 	}
+*/
+	function auto_objects()
+	{
+		$admin_main_class = $this->main_class();
+		$real_main_class = call_user_func(array($admin_main_class, 'extends_class'));
+		return array(
+			'admin_object' => "$admin_main_class(id)",
+			'real_object' => "$real_main_class(id)",
+		);
+	}
 
-	function template_local_vars() { return parent::template_local_vars().($this->id() ? ' object' : ''); }
+	function template_local_vars() { return parent::template_local_vars().($this->id() ? ' admin_object real_object' : ''); }
 }

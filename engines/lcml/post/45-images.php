2125451ac606925dcc5c4001eeab38740c5cf1a6
Balancer <balancer@balancer.ru>
2010-10-07 22:27:39 +0400

    Мелочи.

--- engines/lcml/post/45-images.php
+++ engines/lcml/post/45-images.php
@@ -5,15 +5,15 @@
 			return $txt;
 
 		$n=50;
-		while(preg_match("!\[([http://\w\.\-\+%_/:&\?=#]+\.(jpg|jpeg|gif|png))([^\]]*)\]!ie", $txt, $m) && $n-->0)
+		while(preg_match("!\[([http://\w\.\-\+%_/:&\?=#]+\.(jpg|jpeg|gif|png|sjpg))([^\]]*)\]!ie", $txt, $m) && $n-->0)
 			$txt = str_replace($m[0], lcml("[img \"{$m[1]}\" noflow {$m[3]}]"), $txt);
 
 		$n=50;
-		while(preg_match("!(^|\s)(http://\S+\.(jpg|png|gif|jpeg))(?=($|\s))!ime", $txt, $m) && $n-->0)
+		while(preg_match("!(^|\s)(http://\S+\.(jpg|png|gif|jpeg|sjpg))(?=($|\s))!ime", $txt, $m) && $n-->0)
 			$txt = str_replace($m[0], $m[1].lcml("[img \"{$m[2]}\" left noflow|Взято [url \"{$m[2]}\"|тут]]"), $txt);
 
 		$n=50;
-		while(preg_match("!\[([\w/]+.(jpg|jpeg|gif|png))([^\]]*)\]!ie", $txt, $m) && $n-->0)
+		while(preg_match("!\[([\w/]+.(jpg|jpeg|gif|png|sjpg))([^\]]*)\]!ie", $txt, $m) && $n-->0)
 			$txt = str_replace($m[0], lcml("[img \"{$m[1]}\" noflow {$m[3]}]"), $txt);
 
 		return $txt;
--- engines/lcml/pre/50-auto_images.php
+++ engines/lcml/pre/50-auto_images.php
@@ -6,9 +6,9 @@ function lcml_auto_images($txt)
 		return $txt;
 
 	$txt = preg_replace("!\[img\](.+?)\[/img\]!i", "[img $1 ]'", $txt);
-	$txt = preg_replace("!\[img\s*src=(.+?\.(jpg|png|gif|jpeg))\]!i", "[img $1 ]", $txt);
-	$txt = preg_replace("!\[([^\|\]\s]+?\.(jpg|png|gif|jpeg))\|([^\]]+?)\]!is", "[img $1 468x468 left noflow| $3 ]", $txt);
-	$txt = preg_replace("!\[([^\|\]]+?\.(jpg|png|gif|jpeg))\]!i", "[img $1 468x468 left noflow]", $txt);
+	$txt = preg_replace("!\[img\s*src=(.+?\.(jpg|png|gif|jpeg|sjpg))\]!i", "[img $1 ]", $txt);
+	$txt = preg_replace("!\[([^\|\]\s]+?\.(jpg|png|gif|jpeg|sjpg))\|([^\]]+?)\]!is", "[img $1 468x468 left noflow| $3 ]", $txt);
+	$txt = preg_replace("!\[([^\|\]]+?\.(jpg|png|gif|jpeg|sjpg))\]!i", "[img $1 468x468 left noflow]", $txt);
 
 	return $txt;
 }

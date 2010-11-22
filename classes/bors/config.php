c58ea8ee089e56765eadecfbb13d92ebed09daac
Balancer <balancer@balancer.ru>
2010-11-23 02:53:07 +0300

    Ох, сколько всякой фигни... Давно с этой машины не коммитился. Неоттестированный find_all() для storage_fs_separate. Немного документации. Плюс много всяких мелочей.

--- classes/bors/config.php
+++ classes/bors/config.php
@@ -1,3 +1,14 @@
 <?php
 
-class bors_config extends base_config {}
+class bors_config extends base_config
+{
+	var $object_data = array();
+
+	function __construct(&$object)
+	{
+		foreach($this->object_data() as $key => $value)
+			$object->set($key, $value, false);
+
+		parent::__construct($object);
+	}
+}

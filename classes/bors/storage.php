a72efa4ec54b64df3e440ce8d8461ab39d5ac417
Balancer <balancer@balancer.ru>
2010-11-23 15:21:26 +0300

    Ещё немного причёсывания.

--- classes/bors/storage.php
+++ classes/bors/storage.php
@@ -2,4 +2,10 @@
 
 class bors_storage extends base_null
 {
+	function load($object) { bors_throw(ec('Этот метод ещё не реализован')); }
+	function save($object) { bors_throw(ec('Этот метод ещё не реализован')); }
+	function create($object) { bors_throw(ec('Этот метод ещё не реализован')); }
+	function delete($object) { bors_throw(ec('Этот метод ещё не реализован')); }
+	function find_first($object) { bors_throw(ec('Этот метод ещё не реализован')); }
+	function find_all($object) { bors_throw(ec('Этот метод ещё не реализован')); }
 }

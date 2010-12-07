356ff16011ce39a7bf9cf2aa54b1f990d9d7500d
Balancer <balancer@balancer.ru>
2010-12-07 20:09:53 +0300

    Ещё небльшие правки времени

--- inc/datetime.php
+++ inc/datetime.php
@@ -129,12 +129,12 @@ function part_date($date, $int = false, $on_empty_text = '')
 	{
 		$year = substr($date, 0, 4);
 		$date = substr($date, 4);
-		if($date)
+		if($date != 0)
 		{
 			$month = substr($date, 0, 2);
 			$date = substr($date, 2);
 		}
-		if($date)
+		if($date != 0)
 		{
 			$day = substr($date, 0, 2);
 			$date = substr($date, 2);
@@ -145,12 +145,12 @@ function part_date($date, $int = false, $on_empty_text = '')
 	else
 		list($year, $month, $day) = explode('-', $date);
 
-	if(!$year)
+	if($year == 0)
 		return $on_empty_text;
 
-	if(!$month)
+	if($month == 0)
 		return $year.ec(' г.');
-	if(!$day)
+	if($day == 0)
 		return month_name($month).' '.$year.ec(' г.');
 
 	return $day.' '.month_name_rp($month).' '.$year.ec(' г.');

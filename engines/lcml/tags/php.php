bbc594566275db3c70b22bd0b53cbec38b374782
Balancer <balancer@balancer.ru>
2010-12-16 13:54:15 +0300

    Тэг [php]...[/php] теперь служит благим целям.

--- engines/lcml/tags/php.php
+++ engines/lcml/tags/php.php
@@ -1,14 +1,42 @@
 <?php
 
-function lp_php($txt,$params)
-    {
-    	return $txt; // Блокировано в усмерть.
-//        if(!check_lcml_access('usephp', true))
-            return $txt;
+if(!config('geshi_include'))
+	return;
 
-//		if(user_data('level')<4)
-			return $txt;
+/**
+	[php]echo 2*2;[/php] - прямой строчный PHP-код
 
-        $txt = save_format($txt);
-        return "<?php $txt ?>";
-    }
+	При проверке на многострочность в случае исправлений проверить однострочность на
+	http://balancer.ru/support/2010/12/t75934--voprosy-klassifikatsii-obektov.6737.html
+*/
+
+include_once(config('geshi_include'));
+
+function lp_php($text, $params)
+{
+	$text = restore_format($text);
+//	$text = html_entity_decode($text, ENT_NOQUOTES);
+
+	$lines = count(explode($text, "\n"));
+	$geshi = new GeSHi($text, NULL);
+	$geshi->set_language('PHP');
+	$geshi->set_encoding('UTF-8');
+	$geshi->set_header_type(GESHI_HEADER_NONE);
+	if($lines > 1)
+	{
+		$geshi->enable_classes();
+		$geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS);
+		$geshi->set_overall_class('code');
+	}
+	else
+	{
+		$geshi->enable_line_numbers(GESHI_NO_LINE_NUMBERS);
+		$geshi->set_overall_class('');
+	}
+
+	$geshi->set_use_language_tab_width(true);
+
+	$highlighted_code = $geshi->parse_code();
+
+	return "<tt class=\"code\">{$highlighted_code}</tt>";
+}

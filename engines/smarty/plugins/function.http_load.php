278e3d170fbdb6c4d18a4526762171629e00349d
Balancer <balancer@balancer.ru>
2010-12-20 14:01:12 +0300

    ++

--- engines/smarty/plugins/function.http_load.php
+++ engines/smarty/plugins/function.http_load.php
@@ -5,7 +5,7 @@ function smarty_function_http_load($params, &$smarty)
 	$url = defval($params, 'url');
 	$html = bors_lib_http::get($url);
 	if($charset = defval($params, 'charset'))
-		$html = dc($html, $charset);
+		$html = ec($html, $charset);
 
 	return $html;
 }

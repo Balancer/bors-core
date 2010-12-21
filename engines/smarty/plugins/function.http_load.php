cd032dca4d71d8b526b703b979476f56307a1d4d
Balancer <balancer@balancer.ru>
2010-12-21 20:25:37 +0300

    ++

--- engines/smarty/plugins/function.http_load.php
+++ engines/smarty/plugins/function.http_load.php
@@ -3,8 +3,9 @@
 function smarty_function_http_load($params, &$smarty)
 {
 	$url = defval($params, 'url');
-	$html = bors_lib_http::get($url);
-	if($charset = defval($params, 'charset'))
+	$charset = defval($params, 'charset');
+	$html = bors_lib_http::get($url, $charset);
+	if($charset)
 		$html = ec($html, $charset);
 
 	return $html;

f30e66889ead62b29c63529a32f9928855028e28
Balancer <balancer@balancer.ru>
2011-01-19 14:54:03 +0300

    ++

---
+++ install/db/cache/cache.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `cache` (
+  `type` bigint(20) unsigned DEFAULT NULL,
+  `key` bigint(20) unsigned DEFAULT NULL,
+  `uri` bigint(20) unsigned DEFAULT NULL,
+  `hmd` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `value` longblob,
+  `access_time` int(10) unsigned DEFAULT NULL,
+  `count` int(10) unsigned DEFAULT NULL,
+  `create_time` int(10) unsigned DEFAULT NULL,
+  `expire_time` int(10) unsigned DEFAULT NULL,
+  `rate` float DEFAULT NULL,
+  `saved_time` float DEFAULT NULL,
+  PRIMARY KEY (`hmd`),
+  KEY `type` (`type`),
+  KEY `key` (`key`),
+  KEY `access_time` (`access_time`),
+  KEY `rate` (`rate`),
+  KEY `create_time` (`create_time`),
+  KEY `expire_time` (`expire_time`),
+  KEY `uri` (`uri`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;

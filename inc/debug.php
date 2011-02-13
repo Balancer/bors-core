954a97995b50b6852a50512a537a3dfe482b9f83
Balancer <balancer@balancer.ru>
2011-02-13 23:51:04 +0300

    ++

--- inc/debug.php
+++ inc/debug.php
@@ -44,6 +44,27 @@ function debug_pre($text)
 }
 
 function print_d($data, $string=false) { return debug_xmp(print_r($data, true), $string); }
+function print_dd($data, $string=false, $level=0)
+{
+	$s = '';
+	$step = str_repeat(' ', $level*4);
+	if(is_object($data))
+		$s .= $step.$data->debug_title()."\n";
+	elseif(is_array($data))
+	{
+		$s .= "{$step}array(\n";
+		foreach($data as $key => $value)
+			$s .= $step."    '{$key}' => " . print_dd($value, true, $level+1) . "\n";
+		$s .= "{$step});\n";
+	}
+	else
+		$s .= $step.$data."\n";
+
+	if($string)
+		return trim($s);
+
+	echo $s;
+}
 
 function set_loglevel($n, $file=false)
 {

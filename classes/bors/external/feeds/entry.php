7693d86154a9b43bbde9c069cb9a75b44a8aa8b9
Balancer <balancer@balancer.ru>
2011-02-14 15:55:15 +0300

    vimeo.

--- classes/bors/external/feeds/entry.php
+++ classes/bors/external/feeds/entry.php
@@ -53,6 +53,7 @@ class bors_external_feeds_entry extends base_object_db
 //			exit("$text");
 			$text = preg_replace('!<a href="http://www.youtube.\w+/watch\?v=([^"]+)" rel="nofollow">youtube.com</a>!', '[youtube]$1[/youtube]', $text);
 			$text = preg_replace('!<a href="http://smotri.com/video/view/\?id=([^"]+)" rel="nofollow">smotri.com</a>!', '[smotricom]$1[/smotricom]', $text);
+			$text = preg_replace('!<a href="http://vimeo.com/(\d+)" rel="nofollow">vimeo.com</a>!', '[vimeo]$1[/vimeo]', $text);
 		}
 
 		$text = html2bb(bors_close_tags($text), array('origin_url' => $link, 'strip_forms' => true));

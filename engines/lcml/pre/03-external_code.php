2b2f8044fbe90af8a8113e74beb6f991a49fffde
Balancer <balancer@balancer.ru>
2011-03-09 17:45:04 +0300

    ++

--- engines/lcml/pre/03-external_code.php
+++ engines/lcml/pre/03-external_code.php
@@ -6,8 +6,8 @@ function lcml_external_code($text)
 	if(!config('lcml_external_parse_youtube_disable'))
 	{
 		$text = preg_replace('!<object[^>]+><param[^>]+value="http://www\.youtube\.com/v/([^&?]+).*?</object>!s', "\n[youtube]$1[/youtube]\n", $text);
-		$text = preg_replace('!(^|\s)http://www\.youtube\.com/watch\?v=([^&]+)&\S+(\s|$)!m', "\n[youtube]$2[/youtube]\n", $text);
-		$text = preg_replace('!(^|\s)http://www\.youtube\.com/watch\?v=(\S+)(\s|$)!m', "\n[youtube]$2[/youtube]\n", $text);
+		$text = preg_replace('!(^|\s)http://www\.youtube\.com/watch\?v=([\w\-]+)&\S+(\s|$)!im', "\n[youtube]$2[/youtube]\n", $text);
+		$text = preg_replace('!(^|\s)http://www\.youtube\.com/watch\?v=([\w\-]+)(\s|$)!m', "\n[youtube]$2[/youtube]\n", $text);
 		// http://www.youtube.com/watch?v=TXxcR3qgyYQ&playnext=1&list=PL21AA194D7FBBA2D9
 		$text = preg_replace('!(^|\s)http://www.youtube.com/watch?v=([^&]+)&playnext=\d+&list=\w+(\s|$)!m', "\n[youtube]$2[/youtube]\n", $text);
 	}

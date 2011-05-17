485f1382f732f30cfead0b3e138c5ac84d02d5b5
Balancer <balancer@balancer.ru>
2011-05-18 02:48:40 +0400

    ++

--- classes/bors/cache/smart.php
+++ classes/bors/cache/smart.php
@@ -1,26 +1,31 @@
 <?php
 
-class cache_smart extends cache_base
+class bors_cache_smart extends bors_cache_base
 {
 	private $dbh;
 	private $create_time;
 	private $expire_time;
 
-	function get($type, $key, $uri='', $default=NULL)
+	function get($type, $key, $default = NULL, $object = NULL)
 	{
-		$this->init($type, $key, $uri);
+		$this->type = base_convert(substr(md5($type), 16), 16, 10);
+		$this->key  = base_convert(substr(md5($key), 16), 16, 10);
+		$this->uri  = $object ? base_convert(substr(md5($object->internal_uri_ascii()), 16), 16, 10) : 0;
+		$this->hmd  = base_convert(substr(md5("$type:$key"), 16), 16, 10);
+
+		$this->start_time = microtime(true);
 
 		if(config('cache_disabled'))
 			return $this->last = $default;
 
 		debug_count_inc('smart_cache_gets_total');
 
-		if($x = global_key('cache', $this->last_hmd))
+		if($x = global_key('cache', $this->hmd))
 			return $this->last = $x;
 
 		if($memcache = config('memcached_instance'))
 		{
-			if($x = @$memcache->get('phpmv3'.$this->last_hmd))
+			if($x = @$memcache->get('phpmv4'.$this->hmd))
 			{
 				debug_count_inc('smart_cache_gets_memcached_hits');
 				return $this->last = $x;
@@ -28,8 +33,10 @@ class cache_smart extends cache_base
 		}
 
 		$dbh = new driver_mysql(config('cache_database'));
-		$row = $dbh->select('cache', '*', array('raw hmd' => $this->last_hmd));
-		$dbh->close(); $dbh = NULL;
+		$row = $dbh->select('cache', '*', array('raw hmd' => $this->hmd));
+		$dbh->close();
+		$dbh = NULL;
+
 		$this->last = $row['value'] ? @unserialize($row['value']) : $row['value'];
 
 		$now = time();
@@ -49,7 +56,7 @@ class cache_smart extends cache_base
 		{
 			@$GLOBALS['bors_stat_smart_cache_gets_db_hits']++;
 			$dbh = new driver_mysql(config('cache_database'));
-			$dbh->update('cache', array('hmd'=>$this->last_hmd), array (
+			$dbh->update('cache', array('hmd'=>$this->hmd), array (
 				'int access_time' => $now, 
 				'int count' => $new_count,
 				'float rate' => $rate,
@@ -59,7 +66,7 @@ class cache_smart extends cache_base
 
 			if($memcache = config('memcached_instance'))
 			{
-				$memcache->set('phpmv3'.$this->last_hmd, $this->last, MEMCACHE_COMPRESSED, $this->expire_time - time()+1);
+				$memcache->set('phpmv4'.$this->hmd, $this->last, MEMCACHE_COMPRESSED, $this->expire_time - time()+1);
 				debug_count_inc('smart_cache_gets_memcached_updates');
 			}
 		}
@@ -69,17 +76,16 @@ class cache_smart extends cache_base
 
 	function set($value, $time_to_expire = 86400, $infinite = false)
 	{
-//		if(config('is_debug')) echo "set($value, $time_to_expire, $infinite)<br/>";
+		$do_time = microtime(true) - $this->start_time;
 
-//		echo "cd = ".config('cache_disabled')."<br/>";
 		if(config('cache_disabled'))
 			return $this->last = $value;
 
-		set_global_key('cache', $this->last_hmd, $value);
+		set_global_key('cache', $this->hmd, $value);
 		// Если время хранения отрицательное - используется только memcached, при его наличии.
 		if($memcache = config('memcached_instance'))
 		{
-			$memcache->set('phpmv3'.$this->last_hmd, $value, MEMCACHE_COMPRESSED, abs($time_to_expire));
+			$memcache->set('phpmv4'.$this->hmd, $value, MEMCACHE_COMPRESSED, abs($time_to_expire));
 			debug_count_inc('smart_cache_gets_memcached_stores');
 
 			if($time_to_expire < 0)
@@ -88,20 +94,18 @@ class cache_smart extends cache_base
 
 		$time_to_expire = abs($time_to_expire);
 
-		$do_time = microtime(true) - $this->start_time;
-
 //TODO: сделать настройку отключения. А то мусорит в логах
 //		if($do_time < 0.01 && $time_to_expire > 0)
 //			debug_hidden_log('cache-not-needed', $do_time);
 
-		if($time_to_expire > 0 && $do_time > 0.01)
+		if($time_to_expire > 0/* && $do_time > 0.01*/)
 		{
 			$dbh = new driver_mysql(config('cache_database'));
     		$dbh->replace('cache', array(
-				'int hmd'	=> $this->last_hmd,
-				'int type'	=> $this->last_type,
-				'int key'	=> $this->last_key,
-				'int uri'	=> $this->last_uri,
+				'int hmd'	=> $this->hmd,
+				'int type'	=> $this->type,
+				'int key'	=> $this->key,
+				'int uri'	=> $this->uri,
 				'value'	=> serialize($value),
 				'int access_time' => 0,
 				'int create_time' => $infinite ? -1 : time(),
@@ -110,7 +114,8 @@ class cache_smart extends cache_base
 				'float saved_time' => $do_time,
 				'float rate' => 0,
 			));
-			$dbh->close(); $dbh = NULL;
+			$dbh->close();
+			$dbh = NULL;
 		}
 
 		return $this->last = $value;

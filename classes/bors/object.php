bc8ee32545e9ec0ba529c5301b82a64446e46b5d
Balancer <balancer@balancer.ru>
2011-06-20 01:17:56 +0400

    По мелочи перенос домашних наработок.

--- classes/bors/object.php
+++ classes/bors/object.php
@@ -3,6 +3,21 @@
 // Идёт процесс рефакторинга с переносом функционала base_object в bors_object
 class bors_object extends base_object
 {
+	// Общая структура имён
+	// show() - показывает объект, с кешированием и прочим.
+	// action() - обрабатывает результаты работы форм.
+	// content() - возвращает полное содержимое объекта для вывода в браузер. Некешированное.
+	//			Для страниц использует content_render(), использующий content_template().
+	// body() - возвращает в случае page-классов содержимое внутренней части страницы
+	//			Для страниц использует body_render(), использующий body_template().
+	// html() - возвращает код для вставки объекта в виде HTML.
+	// storage() - возвращает бэкенд класса
+	// url() - ссылка на объект
+	// link() - HTML-текст ссылки на объект
+
+	// Генерируемые классы:
+	//	bors_user - класс пользователя по умолчанию.
+
 	// Предустановленные автообъекты
 	function auto_objects()
 	{
@@ -16,8 +31,30 @@ class bors_object extends base_object
 	function auto_targets()
 	{
 		return array_merge(parent::auto_targets(), array(
-			'target' => 'target_class_name(target_object_id)',
+			'target' => 'target_class_name(target_id)',
 		));
+	}
+
+	function show()
+	{
+		return false; // Пока ничего автоматом не выводим
+		//TODO: добавить debug-info в конец и т.п. вещи из main.php
+//		echo $this->content();
+//		return true;
+	}
+
+	// возвращает полное содержимое объекта для вывода в браузер. Некешированное.
+	function __content() // пока не используется, т.к. более древнее в base_object
+	{
+		if(($render_class = $this->render_class()))
+		{
+			if($render_class == 'self')
+				$render = $this;
+			elseif(!($render = bors_load($render_class)))
+				debug_exit("Can't load global render engine {$render_class} for object '{$object}'");
+
+			return $render->rendering($this);
+		}
 	}
 
 	function renderer()

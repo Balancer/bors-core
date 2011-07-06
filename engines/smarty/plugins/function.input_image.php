368f28f2654c7a940afd8f97c59f576401660965
Balancer <balancer@balancer.ru>
2011-07-06 19:26:20 +0400

    Большая доработка Smarty {input_image} и превьюшек.

--- engines/smarty/plugins/function.input_image.php
+++ engines/smarty/plugins/function.input_image.php
@@ -4,8 +4,11 @@ function smarty_function_input_image($params, &$smarty)
 {
 	extract($params);
 	$image_name_field = defval($params, 'image', 'image');
-	$image_id_field   = defval($params, 'image', $image_name_field.'_id');
-	$image_class_name = defval($params, 'image_class', config('image_class', 'bors_image')); //TODO: убедиться в унификации конфига
+	$image_id_field   = defval($params, 'image_id_field', $image_name_field.'_id');
+	$image_class_name_field   = defval($params, 'image_class_name_field', $image_name_field.'_class_name');
+	$image_class_id_field   = defval($params, 'image_class_id_field', $image_name_field.'_class_id');
+
+	$image_class_name = defval($params, 'class_name', config('image_class_name', 'bors_image')); //TODO: убедиться в унификации конфига
 
 	$obj = $smarty->get_template_vars('form');
 
@@ -28,8 +31,9 @@ function smarty_function_input_image($params, &$smarty)
 		if(!empty($$p))
 			echo " $p=\"{$$p}\"";
 	echo " /><br/>\n";
+
 	$fls = base_object::template_data('form_file_vars');
-	$fls[] = "{$image_name_field}={$image_class_name}({$image_id_field})";
+	$fls[] = "{$image_name_field}={$image_class_name_field}/{$image_class_id_field}({$image_class_name}/{$image_id_field})";
 	base_object::add_template_data('form_file_vars', $fls);
 
 	echo "<input type=\"hidden\" name=\"{$image_name_field}___upload_dir\" value=\"".defval($params, 'upload_dir', config('upload_dir').'/images')."\"/>\n";

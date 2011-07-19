9f72abeafa8751c56e2cc5bb412841bdc3e85203
Balancer <balancer@balancer.ru>
2011-07-19 19:55:40 +0400

    Тесты с Mongo.

--- classes/bors/storage/mongo.php
+++ classes/bors/storage/mongo.php
@@ -41,4 +41,18 @@ class bors_storage_mongo extends bors_storage
 
 		return $obj;
 	}
+
+	function create($object)
+	{
+		$m = new Mongo();
+		$db = $m->selectDB('BORS');
+		$c = $db->createCollection('objects');
+		$c->insert(array_merge(array(
+			'_id' => $object->internal_uri_ascii(),
+			'class_name' => $object->class_name(),
+			'object_id' => $object->id()
+		), $object->data));
+
+		return $object;
+	}
 }

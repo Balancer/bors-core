563e0adcbe501c0e658b3501e4faacb7ea2f6255
Balancer <balancer@balancer.ru>
2011-08-18 17:14:08 +0400

    Зачатки Smart-технологий: http://bors.balancer.ru/_bors/doc/smart/

--- classes/bors/page.php
+++ classes/bors/page.php
@@ -19,4 +19,20 @@ class bors_page extends base_page
 	function body_class() { return $this->body_engine(); }
 	// Для совместимости
 	function body_engine() { return 'bors_bodies_page'; }
+
+	function is_smart() { return true; }
+
+	function pre_show()
+	{
+		if($this->is_smart())
+		{
+			$class_file_base = str_replace('.php', '', $this->class_file());
+			if(file_exists($f="$class_file_base.inc.css"))
+				$this->add_template_data_array('style', file_get_contents($f));
+			if(file_exists($f="$class_file_base.inc.js"))
+				$this->add_template_data_array('javascript', file_get_contents($f));
+		}
+
+		return parent::pre_show();
+	}
 }

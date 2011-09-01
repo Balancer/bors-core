9b359a39107c3b105d7b1811e6960858fec96810
Balancer <balancer@balancer.ru>
2011-09-01 19:32:01 +0400

    Пробуем метод «разработки в процессе написания README».
    Описываю первый простой урок и параллельно под него пишу Sqlite-бэкенд.
    А под него, соответственно, адаптирую старую сырую PDO-рыбу.
    
    http://bors.balancer.ru/_bors/doc/examples/simple-blog/
    
    Обсуждение на http://balancer.ru/support/2011/09/t82982--urok-1-prostejshij-blog-na-sqlite-bekende.779.html

--- engines/lcml/pre/00-code_formats.php
+++ engines/lcml/pre/00-code_formats.php
@@ -4,5 +4,8 @@ function lcml_code_formats($text)
 {
 	$text = preg_replace("!\{\{\{(\w+)(\s|\n)(.+?)\}\}\}!s", '[code $1]$3[/code]', $text);
 	$text = preg_replace('!\{\{\{(.+?)\}\}\}!s', '[code]$1[/code]', $text);
+
+	$text = preg_replace("!\[code=(\w+)\]!s", '[code $1]', $text);
+
 	return $text;
 }

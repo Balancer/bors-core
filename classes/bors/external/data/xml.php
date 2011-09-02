cd42f764b4c7fd273d2aa1775bae98066e2a76d9
Balancer <balancer@balancer.ru>
2011-09-02 18:46:00 +0400

    Доработка демо, xml-бэкенда, pdo.

--- classes/bors/external/data/xml.php
+++ classes/bors/external/data/xml.php
@@ -16,6 +16,9 @@ class bors_external_data_xml extends bors_object_data
 
 		foreach($object->xml_map() as $property => $idx)
 		{
+			if(is_numeric($property))
+				$property = $idx;
+
 			if(preg_match('/^(.+)\|(.+)$/', $idx, $m))
 			{
 				$idx = $m[1];
@@ -77,5 +80,57 @@ class bors_external_data_xml extends bors_object_data
 		}
 
 		return $result;
+	}
+
+	function load_array($object, $where)
+	{
+		$objects = array();
+		$class_name = $object->class_name();
+
+		$xml = bors_lib_http::get_cached($this->xml_url(), $object->xml_cache_ttl());
+		$data = bors_lib_xml::xml2array($xml);
+
+		$data = self::_array_path($data, $this->xml_root());
+		if(!$data)
+			return $object->set_loaded(false);
+//		print_dd($data);
+
+		foreach($data as $item)
+		{
+			foreach($object->xml_map() as $property => $idx)
+			{
+				if(is_numeric($property))
+					$property = $idx;
+
+				if(preg_match('/^(.+)\|(.+)$/', $idx, $m))
+				{
+					$idx = $m[1];
+					$func = $m[2];
+					if(preg_match('/^(.+)\((.*)\)$/', $func, $mm))
+					{
+						$func = NULL;
+						$eval = "\$val={$mm[1]}(\$val,{$mm[2]});";
+//						echo "eval $eval\n";
+					}
+				}
+				else
+					$func = NULL;
+
+				$val = self::_array_path($item, $idx.'/0/cdata');
+				if($func)
+					$val = $func($val);
+
+				if(!empty($eval))
+					eval($eval);
+
+				$object->set($property, $val, false);
+			}
+
+			$object->set_loaded(true);
+			$objects[] = $object;
+			$object = new $class_name(NULL);
+		}
+
+		return $objects;
 	}
 }

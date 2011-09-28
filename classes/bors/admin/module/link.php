2b62056e7c677eaf668dcfb02ff656ce47b2c152
Balancer <balancer@balancer.ru>
2011-09-29 00:06:37 +0300

    Диагностика ошибки при вызове модуля связей в отсутствии объекта.

--- classes/bors/admin/module/link.php
+++ classes/bors/admin/module/link.php
@@ -5,6 +5,9 @@ class bors_admin_module_link extends bors_module
 	function body_data()
 	{
 		$target = $this->arg('object', bors()->main_object());
+		if(!$target)
+			bors_throw(ec('Попытка вызвать модуль связей для несуществующего объекта.'));
+
 		$types = $this->arg('types', '-bors_image,-bors_file');
 		return array_merge(parent::body_data(), array(
 			'target' => $target,

2bcbc7947d7a62795c486c5f540dc06ae01a0fe5
Balancer <balancer@balancer.ru>
2011-11-07 18:33:43 +0300

    ++

--- classes/bors/admin/main.php
+++ classes/bors/admin/main.php
@@ -6,4 +6,12 @@ class bors_admin_main extends base_page
 	function nav_name() { return ec('администрирование'); }
 	function can_cache() { return false; }
 	function admin() { return false; }
+
+	function template()
+	{
+		if(!bors()->user() && ($tpl = config('admin_login_template')))
+			return $tpl;
+
+		return config('admin_template', 'default');
+	}
 }

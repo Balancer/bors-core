76346b90cd3997412d73dfe898cf9dabb1755020
Balancer <balancer@balancer.ru>
2011-11-08 15:43:34 +0300

    Чистые .jpg, всё же, утягиваем даже в [img].

--- engines/lcml/pre/40-classic_bb_img.php
+++ engines/lcml/pre/40-classic_bb_img.php
@@ -2,6 +2,9 @@
 
 function lcml_classic_bb_img($txt)
 {
+	// простые .jpg отрабатываем с утягиванием:
+	$txt = preg_replace("!\[img\]([^\[&\?]+\.jpe?g)\[/img\]!is", "[img $1]", $txt);
+
 	$txt = preg_replace("!\[img\]\s*(.+?)\s*\[/img\]!is", "<img src=\"$1\" alt=\"\" />", $txt);
 	$txt = preg_replace("!\[img=\s*([^]]+?)\s*\]!is", "<img src=\"$1\" alt=\"\" />", $txt);
 

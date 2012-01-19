e9c7a5c7080bd4e61a94cdc643c37567f4d564cc
Balancer <balancer@balancer.ru>
2012-01-19 16:50:30 +0400

    Сносы под нативную поддержку [tag=value]

--- engines/lcml/pre/40-classic_bb_img.php
+++ engines/lcml/pre/40-classic_bb_img.php
@@ -2,13 +2,14 @@
 
 function lcml_classic_bb_img($txt)
 {
-	// простые .jpg отрабатываем с утягиванием:
-	$txt = preg_replace("!\[url=([^\]]+)\]\[img\]([^\[&\?]+\.jpe?g)\[/img\]\[/url\]!is", "[img $2]// $1\n", $txt);
-	$txt = preg_replace("!\[img\]([^\[&\?]+\.jpe?g)\[/img\]!is", "[img $1]", $txt);
-	$txt = preg_replace("!\[img\](http://pics.livejournal.com/.+?\w+)\[/img\]!is", "[img $1]", $txt);
+	// простые .jpg и .png отрабатываем с утягиванием:
+	$txt = preg_replace("!\[url=([^\]]+)\]\[img\]([^\[&\?]+\.(jpe?g|png))\[/img\]\[/url\]!is", "[img=$2]// $1\n", $txt);
+	$txt = preg_replace("!\[img\]([^\[&\?]+\.(jpe?g|png))\[/img\]!is", "[img=$1]", $txt);
+
+	// Утягиваем также ЖЖ-шные картинки
+	$txt = preg_replace("!\[img\](http://pics.livejournal.com/.+?\w+)\[/img\]!is", "[img=$1]", $txt);
 
 	$txt = preg_replace("!\[img\]\s*(.+?)\s*\[/img\]!is", "<img src=\"$1\" alt=\"\" />", $txt);
-	$txt = preg_replace("!\[img=\s*([^]]+?)\s*\]!is", "<img src=\"$1\" alt=\"\" />", $txt);
 
 	return $txt;
 }

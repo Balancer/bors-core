05e3c17daab6abf395d60dd7ae4dde3d73f99bf6
Balancer <balancer@balancer.ru>
2012-01-21 02:09:44 +0400

    На пробу работа с текстом фиксированной ширины + мелкие исправления.

--- classes/bors/lcml/tag/pair/center.php
+++ classes/bors/lcml/tag/pair/center.php
@@ -12,17 +12,30 @@ class bors_lcml_tag_pair_center extends bors_lcml_tag_pair
 		return "<div style=\"text-align: center !important\">".lcml($text)."</div>";
 	}
 
-//TODO: реализовать свёртку через нарезку по ширине и центровку каждой строки.
-/*
 	function text($text)
 	{
-		$screen_width = 80;
-		return "<div style=\"text-align: center !important\">".lcml($text)."</div>";
+		$width = 75;
+		$result = array();
+		foreach(explode("\n", wordwrap($text, $width)) as $s)
+		{
+			$pad = floor(($width-bors_strlen($s))/2);
+			$result[] = str_repeat(' ', $pad).$s;
+		}
+
+		return join("\n", $result);
 	}
-*/
+
 	static function __unit_test($suite)
 	{
 		$code = '[center]Выводы[/center]';
 		$suite->assertEquals('<div style="text-align: center !important">Выводы</div>', lcml($code));
+
+		$code = '[center]Однажды, в студёную зимнюю пору я из лесу вышел, был сильный мороз. Гляжу, поднимается медленно в гору лошадка, везущая хворосту воз[/center]';
+		$suite->assertEquals(
+'                 Однажды, в студёную зимнюю пору я из лесу
+                     вышел, был сильный мороз. Гляжу,
+                   поднимается медленно в гору лошадка,
+                           везущая хворосту воз'
+		, lcml($code, array('output_type' => 'text')));
 	}
 }

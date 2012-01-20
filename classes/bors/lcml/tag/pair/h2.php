a1bfbe9964efdd9b5f5a26f6e0ba53ced1ac2d6f
Balancer <balancer@balancer.ru>
2012-01-20 18:12:16 +0400

    Тьфу.

--- classes/bors/lcml/tag/pair/h2.php
+++ classes/bors/lcml/tag/pair/h2.php
@@ -5,14 +5,14 @@
 
 class bors_lcml_tag_pair_h2 extends bors_lcml_tag_pair
 {
-	static function html($title, &$params)
+	function html($title, &$params)
 	{
 		$params['skip_around_cr'] = true;
 
 		return "\n\n<h2>".lcml($title)."</h2>\n";
 	}
 
-	static function text($title, $params)
+	function text($title, $params)
 	{
 		return "\n\n$title\n".str_repeat("-", bors_strlen($title))."\n";
 	}

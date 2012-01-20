d843664433e2de3c7f32625cac7ac7af13c2dd1e
Balancer <balancer@balancer.ru>
2012-01-20 18:12:16 +0400

    Тьфу.

--- classes/bors/lcml/tag/pair/h3.php
+++ classes/bors/lcml/tag/pair/h3.php
@@ -5,14 +5,14 @@
 
 class bors_lcml_tag_pair_h3 extends bors_lcml_tag_pair
 {
-	static function html($title, &$params)
+	function html($title, &$params)
 	{
 		$params['skip_around_cr'] = true;
 
 		return "\n\n<h3>".lcml($title)."</h3>\n";
 	}
 
-	static function text($title, $params)
+	function text($title, $params)
 	{
 		return "\n\n*** $title ***\n";
 	}

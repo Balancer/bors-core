590d3c1950912642d289a02d393386b40fd7ff29
Balancer <balancer@balancer.ru>
2012-01-20 17:37:50 +0400

    Простейшие встроенные автотесты. Проверка.

--- classes/bors/lcml/tag/pair/b.php
+++ classes/bors/lcml/tag/pair/b.php
@@ -13,4 +13,11 @@ class bors_lcml_tag_pair_b extends bors_lcml_tag_pair
 	{
 		return "*".$this->lcml($text)."*";
 	}
+
+	static function __unit_test($suite)
+	{
+		$code = '[b]Здавствуй, мир![/b]';
+		$suite->assertEquals('<strong>Здавствуй, мир!</strong>', lcml($code));
+		$suite->assertEquals('*Здавствуй, мир!*', lcml($code, array('output_type' => 'text')));
+	}
 }

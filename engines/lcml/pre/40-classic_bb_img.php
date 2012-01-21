41283dfd0758597df72f8ab0307647530dc65607
Balancer <balancer@balancer.ru>
2012-01-21 12:34:24 +0400

    ++

--- engines/lcml/pre/40-classic_bb_img.php
+++ engines/lcml/pre/40-classic_bb_img.php
@@ -3,7 +3,7 @@
 function lcml_classic_bb_img($txt)
 {
 	// простые .jpg и .png отрабатываем с утягиванием:
-	$txt = preg_replace("!\[url=([^\]]+)\]\[img\]([^\[&\?]+\.(jpe?g|png))\[/img\]\[/url\]!is", "[img=$2]// $1\n", $txt);
+	$txt = preg_replace("!\[url=([^\]]+)\]\[img\]([^\[&\?]+\.(jpe?g|png))\[/img\]\[/url\]!is", "[img=$2] // $1\n", $txt);
 	$txt = preg_replace("!\[img\]([^\[&\?]+\.(jpe?g|png))\[/img\]!is", "[img=$1]", $txt);
 
 	// Утягиваем также ЖЖ-шные картинки

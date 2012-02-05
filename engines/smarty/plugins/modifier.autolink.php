75304f930f49b0abc312c171b7fada55246d0d53
Balancer <balancer@balancer.ru>
2012-02-06 03:37:46 +0400

    email тоже подсвечиваем.

--- engines/smarty/plugins/modifier.autolink.php
+++ engines/smarty/plugins/modifier.autolink.php
@@ -3,5 +3,6 @@
 function smarty_modifier_autolink($text)
 {
 	$text = preg_replace('!^(www\.\S+)$!', "<a href=\"http://$1\">$1</a>", $text);
+	$text = preg_replace('!^(\S+@\S+)$!', "<a href=\"mailto:$1\">$1</a>", $text);
 	return $text;
 }

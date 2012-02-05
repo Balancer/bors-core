61c2a1ec217c9859f5b2a6ac0e822d3c6d844e37
Balancer <balancer@balancer.ru>
2012-02-05 05:20:00 +0400

    Вывод времени в виде d.m.Y

--- classes/bors/time/meta.php
+++ classes/bors/time/meta.php
@@ -5,5 +5,19 @@ class bors_time_meta extends bors_object //TODO: придумать назван
 	var $is_null = false;
 
 	function short($def = '') { return bors_lib_time::short($this->timestamp(), $def); }
-	function full_hdate() { return bors_lib_date::text($this->timestamp()); }
+	function full_hdate()
+	{
+		if($ts = $this->timestamp())
+			return bors_lib_date::text($ts);
+
+		return NULL;
+	}
+
+	function dmy()
+	{
+		if($ts = $this->timestamp())
+			return date('d.m.Y', $ts);
+
+		return NULL;
+	}
 }

0c839375d972124aafa8946037e18b16c42aa097
Balancer <balancer@balancer.ru>
2012-02-17 02:43:10 +0400

    Дорабатываем связи.

--- classes/bors/cross/types.php
+++ classes/bors/cross/types.php
@@ -2,20 +2,24 @@
 
 class bors_cross_types extends base_list
 {
+	const NONE		= '0';
+	const AUTO		= '2';
 	const MENTION	= '2';
 	const ABOUT		= '3';
 	const DELETED	= '4';
 	const LINK		= '5';
+	const INDIRECT	= '6';
 
 	function named_list()
 	{
 		return array(
-			'0' => ec('Не задано'),
-			'1' => ec('Автоматический'),
+			self::NONE		=> ec('Не задано'),
+			self::AUTO		=> ec('Автоматический'),
 			self::MENTION	=> ec('Упоминается'),
 			self::ABOUT		=> ec('Посвящается'),
 			self::DELETED	=> ec('Удалено'), // Сохраняется для подавления автоматических добавлений.
 			self::LINK		=> ec('Привязка'), // обычно при ручном связывании дерева объектов
+			self::INDIRECT	=> ec('Косвенно'), // При автоматической привязке через другие объекты
 		);
 	}
 }

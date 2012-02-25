954550c2aed533c80164947422d8def8ce91664a
Balancer <balancer@balancer.ru>
2012-02-26 02:49:49 +0400

    Убираем дубликат.

--- engines/smarty/plugins/modifier.sklonn.php
+++ engines/smarty/plugins/modifier.sklonn.php
@@ -9,15 +9,5 @@
 
 function smarty_modifier_sklonn($n, $s1, $s2=NULL, $s5=NULL)
 {
-	if($s2 === NULL)
-		list($s1, $s2, $s5) = explode(',', $s1);
-
-    $ns=intval(substr($n,-1));
-    $n2=intval(substr($n,-2));
-
-    if($n2>=10 && $n2<=19) return $n.' '.$s5;
-    if($ns==1) return $n.' '.$s1;
-    if($ns>=2&&$ns<=4) return $n.' '.$s2;
-
-    return $n.' '.$s5;
+    return sklonn($n, $s1, $s2, $s5);
 }

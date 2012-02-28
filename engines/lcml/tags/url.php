b340b6a432f7d03868a54c7a29469c2f02586bdc
Balancer <balancer@balancer.ru>
2012-02-29 02:36:23 +0400

    Исправления парсера lcml.

--- engines/lcml/tags/url.php
+++ engines/lcml/tags/url.php
@@ -6,7 +6,8 @@ function lp_url($text, $params)
 	$url_data = url_parse($url);
 	$external = @$url_data['local'] ? '' : ' class="external"';
 	$blacklist = $external || preg_match('!'.config('seo_domains_whitelist_regexp', $_SERVER['HTTP_HOST']).'!', $url_data['host']);
-	return "<a ".($blacklist ? 'rel="nofollow" ' : '')."href=\"$url\"$external>".lcml($text, array('html'=>'safe', 'only_tags' => true))."</a>";
+	// specialchars для http://balancer.ru/g/p2728134
+	return "<a ".($blacklist ? 'rel="nofollow" ' : '')."href=\"".htmlspecialchars($url)."\"$external>".lcml($text, array('html'=>'safe', 'only_tags' => true))."</a>";
 }
 
 function lt_url($params) 

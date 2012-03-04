c32475c9970bc775189ad59221c9d25037876d07
Balancer <balancer@balancer.ru>
2012-03-04 04:01:37 +0400

    ++

--- classes/bors/core/find.php
+++ classes/bors/core/find.php
@@ -10,11 +10,27 @@ class bors_core_find
 		$this->_class_name = $class_name;
 	}
 
-	// Найти все объекты, соответствующие заданным критериям
-	function all()
+	function first()
 	{
-		return bors_find_all($this->_class_name, $this->_where());
+		return bors_find_first($this->_class_name, $this->_where);
 	}
 
+	// Найти все объекты, соответствующие заданным критериям
+	function all($limit1=NULL, $limit2=NULL)
+	{
+		return bors_find_all($this->_class_name, $this->_where);
+	}
 
+	function count()
+	{
+		return bors_count($this->_class_name, $this->_where);
+	}
+
+	function where($conditions)
+	{
+		if(is_array($conditions))
+			$this->_where = array_merge($this->_where, $conditions);
+		else
+			$this->_where[] = $conditions;
+	}
 }

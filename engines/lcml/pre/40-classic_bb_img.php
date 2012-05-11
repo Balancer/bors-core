dbbcc4d802a505095f12eaf55a7ca4ea261f5769
Balancer <balancer@balancer.ru>
2012-05-11 19:48:29 +0400

    Во избежание.

--- engines/lcml/pre/40-classic_bb_img.php
+++ engines/lcml/pre/40-classic_bb_img.php
@@ -4,12 +4,12 @@ function lcml_classic_bb_img($txt)
 {
 	// простые .jpg и .png отрабатываем с утягиванием:
 //	$txt = preg_replace("!\[url=([^\]]+)\]\[img\]([^\[&\?]+\.(jpe?g|png))\[/img\]\[/url\]!is", "[img=$2]// $1\n", $txt);
-	$txt = preg_replace("!\[img\]([^\[&\?]+\.(jpe?g|png))\[/img\]!is", "[img=$1]", $txt);
+	$txt = preg_replace("!\[img\]((https?|ftp)://[^\[&\?]+\.(jpe?g|png))\[/img\]!is", "[img=$1]", $txt);
 
 	// Утягиваем также ЖЖ-шные картинки
 	$txt = preg_replace("!\[img\](http://pics.livejournal.com/.+?\w+)\[/img\]!is", "[img=$1]", $txt);
 
-	$txt = preg_replace("!\[img\]\s*(.+?)\s*\[/img\]!is", "<img src=\"$1\" alt=\"\" />", $txt);
+	$txt = preg_replace("!\[img\]\s*((https?|ftp)://.+?)\s*\[/img\]!is", "<img src=\"$1\" alt=\"\" />", $txt);
 
 	return $txt;
 }

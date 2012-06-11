f046991c70c3c3ec2ce89a79df8e6ecc23f4c207
Balancer <balancer@balancer.ru>
2012-06-11 05:00:55 +0400

    Улучшение определения разделов по ссылкам и инфраструктура. Доработка use-функций.

--- inc/functions/url/parse.php
+++ inc/functions/url/parse.php
@@ -1,21 +1,5 @@
 <?php
 
-function bors_url_parse($url, $field=NULL, $var=NULL, $default=NULL)
-{
-	$data = parse_url($url);
-	if($field)
-		$data = $data[$field];
-
-	if($var)
-	{
-		$parsed_data = array();
-		parse_str($data, $parsed_data);
-		$data = defval($parsed_data, $var, $default);
-	}
-
-	return $data;
-}
-
 function url_parse($url)
 {
 //	if(preg_match('!^/!', $url))

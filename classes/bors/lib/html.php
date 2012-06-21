569eb05c67dcc6ce2e5aa9270c160b089e932f73
Balancer <balancer@balancer.ru>
2012-06-22 03:38:28 +0400

    Доработка автоматических списков + косметика.

--- classes/bors/lib/html.php
+++ classes/bors/lib/html.php
@@ -56,4 +56,11 @@ class bors_lib_html
 
 		return $url_data['scheme'].'://'.$url_data['host'].$value;
 	}
+
+	static function autolink($text)
+	{
+		$text = preg_replace('!^(www\.\S+)$!', "<a href=\"http://$1\">$1</a>", $text);
+		$text = preg_replace('!^(\S+@\S+)$!', "<a href=\"mailto:$1\">$1</a>", $text);
+		return $text;
+	}
 }

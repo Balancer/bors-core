183149c9a0628bf3b3bc8457f1f21350547750fc
Balancer <balancer@balancer.ru>
2012-09-29 12:44:21 +0400

    Закрытие тэгов через DOM.

--- classes/blib/html.php
+++ classes/blib/html.php
@@ -4,76 +4,22 @@ class blib_html
 {
 	function close_tags($html)
 	{
-		$html = self::close_opened_tags($html);
-		$html = self::close_closed_tags($html);
-		return $html;
-	}
-
-	// via http://rmcreative.ru/blog/post/zakryt-nezakrytye-tegi
-	function close_opened_tags($html)
-	{
-		$single_tags = array('meta','img','br','link','area','input','hr','col','param','base');
-		preg_match_all('~<([a-z0-9]+)(?: [^>]*)?(?<![/|/ ])>~iU', $html, $result);
-		$openedtags = $result[1];
-		preg_match_all('~</([a-z0-9]+)>~iU', $html, $result);
-		$closedtags = $result[1];
-		$len_opened = count($openedtags);
-		$len_closed = count($closedtags);
-
-//		if ($len_closed == $len_opened)
-//			return $html;
-
-		$openedtags = array_reverse($openedtags);
-
-		for ($i=0; $i < $len_opened; $i++)
-		{
-			if (!in_array($openedtags[$i], $single_tags))
-			{
-				if (FALSE !== ($key = array_search($openedtags[$i], $closedtags)))
-					unset($closedtags[$key]);
-				else
-					$html .= '</'.$openedtags[$i].'>';
-			}
-		}
-
-		return $html;
-	}
-
-	function close_closed_tags($html)
-	{
-		$single_tags = array('meta','img','br','link','area','input','hr','col','param','base');
-		preg_match_all('~<([a-z0-9]+)(?: [^>]*)?(?<![/|/ ])>~iU', $html, $result);
-		$openedtags = $result[1];
-		preg_match_all('~</([a-z0-9]+)>~iU', $html, $result);
-		$closedtags = $result[1];
-		$len_opened = count($openedtags);
-		$len_closed = count($closedtags);
-
-//		Нельзя, так как число открытых и закрытых несовпадающих тэгов может быть равно:
-//		<b>test</s>
-//		if ($len_closed == $len_opened)
-//			return $html;
-
-		$openedtags = array_reverse($openedtags);
-
-		for ($i=0; $i < $len_closed; $i++)
-		{
-			if(in_array($closedtags[$i], $single_tags))
-				continue;
-
-			if (FALSE !== ($key = array_search($closedtags[$i], $openedtags)))
-				unset($openedtags[$key]);
-			else
-				$html = '<'.$closedtags[$i].'>' . $html;
-		}
-
-		return $html;
+		$dom = new DOMDocument('1.0', 'utf-8');
+		@$dom->loadHTML('<html><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><body>' . $html . '</body></html>');
+		$html = $dom->saveHTML();
+		$html = preg_replace("!^.+?</head>!is", "", $html);
+		$html = preg_replace("/\<\/?(body|html|p)>/i", "", $html);
+		return trim($html);
 	}
 
 	static function __unit_test($suite)
 	{
-		$suite->assertEquals('<i><b>text</b></i>', blib_html::close_opened_tags('<i><b>text</b>'));
-		$suite->assertEquals('<s><b>text</b></s>', blib_html::close_closed_tags('<b>text</b></s>'));
-		$suite->assertEquals('<div ><blockquote class="q"> test<hr/> </p ></blockquote>', blib_html::close_tags('<div ><blockquote class="q"> test<hr/> </p ></blockquote>'));
+		$suite->assertEquals('<i><b>text</b></i>', blib_html::close_tags('<i><b>text</b>'));
+		$suite->assertEquals('<b>text</b>', blib_html::close_tags('<b>text</b></s>'));
+		$suite->assertEquals('<div><blockquote class="q"> test<hr></blockquote></div>', blib_html::close_tags('<div ><blockquote class="q"> test<hr/> </p ></blockquote>'));
+
+		$suite->assertEquals('<blockquote><div>Бывший министр</div></blockquote>', blib_html::close_tags('<blockquote><div>Бывший министр'));
+
+		$suite->assertEquals(file_get_contents(dirname(__FILE__).'/html.unittest.out.text'), blib_html::close_tags(file_get_contents(dirname(__FILE__).'/html.unittest.data.text')));
 	}
 }

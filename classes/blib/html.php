a3df03985b8bef9bfc0df294970ccb71e84c4428
Balancer <balancer@balancer.ru>
2013-01-12 01:14:07 +0400

    Мелкие исправления + включение каталога собственных плагинов Smarty в общий список, а то с какой-то версии поломали.

--- classes/blib/html.php
+++ classes/blib/html.php
@@ -2,7 +2,16 @@
 
 class blib_html
 {
-	function close_tags($html)
+
+	public static function close_tags($html)
+	{
+		if(function_exists('DOMDocument'))
+			return self::__close_tags_new($html);
+
+		return self::__close_tags_old2($html);
+	}
+
+	private static function __close_tags_new($html)
 	{
 		$dom = new DOMDocument('1.0', 'utf-8');
 		@$dom->loadHTML('<html><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><body>' . $html . '</body></html>');
@@ -10,6 +19,33 @@ class blib_html
 		$html = preg_replace("!^.+?</head>!is", "", $html);
 		$html = preg_replace("/\<\/?(body|html|p)>/i", "", $html);
 		return trim($html);
+	}
+
+	private static function __close_tags_old2($html)
+	{
+	    $single_tags = array('meta','img','br','link','area','input','hr','col','param','base');
+	    preg_match_all('~<([a-z0-9]+)(?: [^>]*)?(?<![/|/ ])>~iU', $html, $result);
+	    $openedtags = $result[1];
+	    preg_match_all('~</([a-z0-9]+)>~iU', $html, $result);
+	    $closedtags = $result[1];
+	    $len_opened = count($openedtags);
+
+	    if (count($closedtags) == $len_opened)
+	        return $html;
+
+	    $openedtags = array_reverse($openedtags);
+	    for ($i=0; $i < $len_opened; $i++)
+	    {
+	        if (!in_array($openedtags[$i], $single_tags))
+	        {
+	            if (FALSE !== ($key = array_search($openedtags[$i], $closedtags)))
+	                unset($closedtags[$key]);
+	            else
+	                $html .= '</'.$openedtags[$i].'>';
+	        }
+	    }
+
+	    return $html;
 	}
 
 	static function __unit_test($suite)

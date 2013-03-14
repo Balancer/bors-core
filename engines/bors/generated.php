62f996dcc87d3df8df4baebf390e8ca615dfc16a
Balancer <balancer@balancer.ru>
2013-03-14 23:58:43 +0400

    Ещё одна обёртка.

--- engines/bors/generated.php
+++ engines/bors/generated.php
@@ -9,6 +9,7 @@ if(function_exists('mb_strtolower') && config('internal_charset') == 'utf-8')
 	function bors_strlen($str) { return mb_strlen($str); }
 	function bors_substr($str, $start, $length=NULL) { return is_null($length) ? mb_substr($str, $start) : mb_substr($str, $start, $length); }
 	function bors_strpos($str, $need, $start=NULL) { return is_null($start) ? mb_strpos($str, $need) : mb_strpos($str, $need, $start); }
+	function bors_strrpos($str, $need, $start=NULL) { return is_null($start) ? mb_strrpos($str, $need) : mb_strrpos($str, $need, $start); }
 	function bors_stripos($str, $need, $start=NULL) { return is_null($start) ? mb_stripos($str, $need) : mb_stripos($str, $need, $start); }
 	function bors_ucfirst($str) { return mb_substr(mb_strtoupper($str), 0, 1).mb_substr(mb_strtolower($str), 1); }
 }
@@ -19,6 +20,7 @@ else
 	function bors_strlen($str) { return strlen($str); }
 	function bors_substr($str, $start, $length=NULL) { return is_null($length) ? substr($str, $start) : substr($str, $start, $length); }
 	function bors_strpos($str, $need, $start=NULL) { return is_null($start) ? strpos($str, $need) : strpos($str, $need, $start); }
+	function bors_strrpos($str, $need, $start=NULL) { return is_null($start) ? strrpos($str, $need) : strrpos($str, $need, $start); }
 	function bors_stripos($str, $need, $start=NULL) { return is_null($start) ? stripos($str, $need) : stripos($str, $need, $start); }
 	function bors_ucfirst($str) { return ucfirst($str); }
 }

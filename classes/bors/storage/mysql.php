a3cdfe1ac948ade5eec7b43e07068308b90c898e
Balancer <balancer@balancer.ru>
2013-04-16 05:38:07 +0400

    Исправления.

--- classes/bors/storage/mysql.php
+++ classes/bors/storage/mysql.php
@@ -649,7 +649,7 @@ class bors_storage_mysql extends bors_storage implements Iterator
 						$new_id = $object->id();
 					if(!$new_id && ($idf = $object->id_field()))
 						$new_id = $object->get($idf);
-					if(!$new_id)
+					if(!$new_id && !$object->ignore_on_new_instance())
 						debug_hidden_log('_orm_error', "Can't get new id on new instance for ".$object->debug_title()."; data=".print_r($object->data, true));
 				}
 			}

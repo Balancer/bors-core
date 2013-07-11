cb576f1b922c334e664cbadcf65cfde2c0f8bd45
Balancer <balancer@balancer.ru>
2013-07-11 05:27:04 +0400

    ++

--- classes/bors/lcml/tag/single/abbr.php
+++ classes/bors/lcml/tag/single/abbr.php
@@ -9,12 +9,19 @@ class bors_lcml_tag_single_abbr extends bors_lcml_tag_single
 {
 	function html($params)
 	{
-		return "<abbr title=\"{$params['description']}\">{$params['orig']}</abbr>";
+		if(empty($params['abbr']))
+			return "<abbr title=\"{$params['description']}\">{$params['orig']}</abbr>";
+
+		return "<abbr title=\"{$params['tail']}\">{$params['abbr']}</abbr>";
 	}
 
 	static function __unit_test($suite)
 	{
+		require_once('engines/lcml.php');
 		$code = '[abbr РХБЗ|Радиационная, химическая и биологическая защита]';
 		$suite->assertEquals('<abbr title="Радиационная, химическая и биологическая защита">РХБЗ</abbr>', lcml($code));
+		$suite->assertEquals(
+			'<abbr title="Радиационная, химическая и биологическая защита">РХБЗ</abbr>',
+			lcml('[abbr=РХБЗ Радиационная, химическая и биологическая защита]'));
 	}
 }

2e435c7e45eb221e3c3bb4a5dd87282917b63659
Balancer <balancer@balancer.ru>
2013-07-29 14:49:29 +0400

    ++

--- classes/bors/system/go/internal.php
+++ classes/bors/system/go/internal.php
@@ -4,9 +4,10 @@ class bors_system_go_internal extends base_object
 {
 	function pre_show()
 	{
-		$object = object_load($this->id());
-//		echo "{$this->id()} => $object";
-		return $object->igo();
+		if($object = object_load($this->id()))
+			return $object->igo();
+
+		return bors_http_error(404);
 	}
 
 	function target() { return object_load($this->id()); }

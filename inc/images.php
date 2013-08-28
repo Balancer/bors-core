0f1e8db29cf3b1c0b917900ecc828002823e638f
Balancer <balancer@balancer.ru>
2013-08-28 05:10:02 +0400

    ++

--- inc/images.php
+++ inc/images.php
@@ -108,8 +108,12 @@ Max=".config('images_resize_max_width')."x".config('images_resize_max_height')."
 //					$imagick->resampleImage($width, $height, Imagick::FILTER_BOX, 1);
 				} while ($imagick->nextImage());
 				$imagick = $imagick->deconstructImages();
-				$imagick->writeImages($file_out, true);
-				@chmod($file_out, 0666);
+				mkpath($d = dirname($file_out), 0777);
+				if(is_dir($d) && is_writable($d))
+				{
+					$imagick->writeImages($file_out, true);
+					chmod($file_out, 0666);
+				}
 				bors_thread_unlock('image_file_scale');
 				return $img->isError();
 			}

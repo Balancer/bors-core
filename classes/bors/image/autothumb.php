74c569add5fe6ba58d3b631f282b88276904ab1e
Balancer <balancer@balancer.ru>
2014-02-03 02:20:56 +0400

    Возможность грохнуть кеш-картинку.

--- classes/bors/image/autothumb.php
+++ classes/bors/image/autothumb.php
@@ -18,6 +18,10 @@ class bors_image_autothumb extends bors_object
 			if(!preg_match('!^(/.*/)(\d*x\d*\([^)]+\))/([^/]+)$!', $thumb_path, $m))
 				return;
 
+		// Если убиваем кеш, то стереть файл
+		if(array_key_exists('nc', $_GET) && file_exists($f = $_SERVER['DOCUMENT_ROOT'].'/cache'.$thumb_path))
+			unlink($f);
+
 		$origin_path = $m[1].$m[3];
 		if(!file_exists($_SERVER['DOCUMENT_ROOT'] . $origin_path))
 		{

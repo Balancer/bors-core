b1885ec63569d49461eae2f76d93d1bfde804e08
Balancer <balancer@balancer.ru>
2014-02-03 06:40:21 +0400

    access log — в память. И храним только 10 минут. Достаточно.

--- install/db/access_log.sql
+++ install/db/access_log.sql
@@ -1,20 +1,24 @@
-CREATE TABLE IF NOT EXISTS `bors_access_log` (
+DROP TABLE IF EXISTS `bors_access_log`;
+
+CREATE TABLE `bors_access_log` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_ip` varchar(16) NOT NULL DEFAULT '',
-  `user_id` int(11) NOT NULL DEFAULT '0',
-  `url` varchar(255) NOT NULL DEFAULT '',
-  `server_uri` varchar(255) NOT NULL DEFAULT '',
-  `referer` varchar(255) NOT NULL DEFAULT '',
-  `class_name` varchar(64) NOT NULL DEFAULT '',
-  `object_id` int(11) NOT NULL DEFAULT '0',
-  `access_time` int(11) NOT NULL DEFAULT '0',
-  `operation_time` float NOT NULL DEFAULT '0',
-  `has_bors` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `has_bors_url` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `user_agent` varchar(255) NOT NULL DEFAULT '',
-  `is_bot` varchar(64) NOT NULL DEFAULT '',
-  `was_counted` tinyint(1) unsigned NOT NULL,
+  `user_ip` varchar(16) NOT NULL,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `url` varchar(4095) NOT NULL,
+  `server_uri` varchar(4095) NOT NULL,
+  `referer` varchar(4095) DEFAULT NULL,
+  `class_name` varchar(64) NOT NULL,
+  `object_id` varchar(255) DEFAULT NULL,
+  `access_time` int(10) unsigned NOT NULL,
+  `operation_time` float NOT NULL,
+  `has_bors` tinyint(1) unsigned NOT NULL,
+  `has_bors_url` tinyint(1) unsigned NOT NULL,
+  `user_agent` varchar(4095) DEFAULT NULL,
+  `is_bot` varchar(64) DEFAULT NULL,
+  `was_counted` tinyint(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `user_ip` (`user_ip`),
-  KEY `uip_at` (`user_ip`,`access_time`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  KEY `access_time` (`access_time`),
+  KEY `was_counted` (`was_counted`),
+  KEY `user_ip___access_time` (`user_ip`,`access_time`)
+) ENGINE=MEMORY DEFAULT CHARSET=utf8;

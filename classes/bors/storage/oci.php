6e0733ac9dd056d6b8d79bcbe4bb47e2446b67ff
Balancer <balancer@balancer.ru>
2014-02-11 04:49:38 +0400

    Работа с oci БД/ORM + мелочи.

--- classes/bors/storage/oci.php
+++ classes/bors/storage/oci.php
@@ -3,7 +3,7 @@
 /**	Oracle backend (oci), R/O version
 */
 
-class bors_storage_oci extends bors_storage
+class bors_storage_oci extends bors_storage implements Iterator
 {
 	function load($object)
 	{
@@ -81,4 +81,87 @@ class bors_storage_oci extends bors_storage
 		return $objects;
 	}
 
+	private $data;
+	private $dbi;
+	private $object;
+	private $__class_name;
+
+	static function each($class_name, $where)
+	{
+		$by_id  = popval($where, 'by_id');
+
+		$select = array();
+		$post_functions = array();
+
+		$object = new $class_name(NULL);
+
+		foreach(bors_lib_orm::main_fields($object) as $f)
+		{
+			$x = $f['name'];
+			if($f['name'] != $f['property'])
+				$x .= " AS `{$f['property']}`";
+
+			$select[] = $x;
+
+			if(!empty($f['post_function']))
+				$post_functions[$f['property']] = $f['post_function'];
+		}
+
+		$class_name = $object->class_name();
+
+		$iterator = new bors_storage_oci();
+		$iterator->__class_name = $class_name;
+
+		$iterator->dbi = driver_oci::factory($object->db_name())->each($object->table_name(), join(',', $select), $where);
+		return $iterator;
+	}
+
+	// void Iterator::rewind ( void )
+	// Rewinds back to the first element of the Iterator.
+	// Any returned value is ignored.
+	// Вызывается первый раз.
+    public function rewind()
+    {
+		$this->dbi->rewind();
+    }
+
+	// void Iterator::next ( void )
+	// Moves the current position to the next element.
+	// Any returned value is ignored.
+	// Вызывается после выдачи первого элемента, перед выдачей следующих
+    public function next()
+    {
+		$this->dbi->next();
+    }
+
+	// boolean Iterator::valid ( void )
+	// This method is called after Iterator::rewind() and Iterator::next() to check if the current position is valid.
+	// The return value will be casted to boolean and then evaluated. Returns TRUE on success or FALSE on failure.
+    public function valid()
+    {
+		return $this->dbi->valid();
+    }
+
+	// mixed Iterator::current ( void )
+	// Returns the current element.
+	// Can return any type.
+    public function current()
+    {
+    	$data = $this->dbi->current();
+		$class_name = $this->__class_name;
+		$object = new $class_name($data['id']);
+//		$object->set_id($data['id']);
+		$object->data = $data;
+		$object->set_is_loaded(true);
+		return $object;
+    }
+
+	// scalar Iterator::key ( void )
+	// Returns the key of the current element.
+	// Returns scalar on success, or NULL on failure.
+	// Not implemented
+    public function key()
+    {
+		return NULL;
+    }
 }

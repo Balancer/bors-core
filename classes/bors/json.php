12b57b6252b80d5567728327eda5f6b1b1d30fd0
Balancer <balancer@balancer.ru>
2014-04-18 04:22:46 +0400

    Фикс json-параметров для PHP < 5.4.0

--- classes/bors/json.php
+++ classes/bors/json.php
@@ -6,7 +6,10 @@ class bors_json extends bors_object
 	{
 		header("Content-type: application/json; charset=".config('output_charset'));
 		config_set('debug.timing', false); // Чтобы не мусорить комментарием в конце JSON.
-		echo json_encode($this->data(), JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+		if(version_compare(PHP_VERSION, '5.4.0') >= 0)
+			echo json_encode($this->data(), JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+		else
+			echo json_encode($this->data(), JSON_NUMERIC_CHECK);
 		return true;
 	}
 }

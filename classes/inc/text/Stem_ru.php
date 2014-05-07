1dbf4fe96c71563f0e4c97e5d26395f0d15236f3
Balancer <balancer@balancer.ru>
2014-05-07 14:50:16 +0400

    Фикс.

--- classes/inc/text/Stem_ru.php
+++ classes/inc/text/Stem_ru.php
@@ -4,4 +4,5 @@ if(class_exists('Lingua_Stem_Ru'))
 	return;
 
 $charset = str_replace('-', '', strtolower(config('internal_charset')));
+require_once(__DIR__.'/Stem_ru-'.$charset.'.php');
 eval("class Lingua_Stem_Ru extends Lingua_Stem_Ru_$charset { }");

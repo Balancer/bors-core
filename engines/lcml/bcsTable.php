a469625fad47e5bfc0fa9fc727f69fe664985fb1
Balancer <balancer@balancer.ru>
2014-06-16 03:14:02 +0400

    Проба перевода генерации таблиц на Anahkiasen/html-object.

--- engines/lcml/bcsTable.php
+++ engines/lcml/bcsTable.php
@@ -1,122 +1,137 @@
 <?php
 
-    require_once('error_log.php');
+//use HtmlObject;
 
-    class bcsTable
-    {
-        var $table_data;
-        var $row_spans;
-        var $col_spans;
-        var $heads;
-        var $rows;
-        var $cols;
-        var $row;
-        var $col;
+class bcsTable
+{
+		var $table_data;
+		var $row_spans;
+		var $col_spans;
+		var $heads;
+		var $rows;
+		var $cols;
+		var $row;
+		var $col;
 
-        var $table_width;
-        var $table_border;
+		var $table_width;
+		var $table_border;
 
-        function rewind()
-        {
-            $this->row = $this->col = 0;
-        }
+		function rewind()
+		{
+			$this->row = $this->col = 0;
+		}
 
-        function __construct($args = array())
-        {
-			var_dump($args);
+		function __construct($args = array())
+		{
+			$this->layout = defval($args, 'layout');
+//			var_dump($layout);
 
-            $this->data      = array();
-            $this->row_spans = array();
-            $this->col_spans = array();
-            $this->heads     = array();
-            $this->rows = $this->cols = 0;
-            $this->rewind();
+			$this->data	  = array();
+			$this->row_spans = array();
+			$this->col_spans = array();
+			$this->heads	 = array();
+			$this->rows = $this->cols = 0;
+			$this->rewind();
 
-            $this->table_width = '';
-            $this->table_border = " class=\"btab\" cellSpacing=\"0\"";
-        }
+			$this->table_width = '';
+		}
 
-        function table_width($width)
-        {
-            $this->table_width = " width=\"$width\" style=\"width: $width\"";
-        }
+		function table_width($width)
+		{
+			$this->table_width = $width;
+		}
 
-        function set_max()
-        {
-            if($this->col >= $this->cols)
-            {
-//                if($this->row > 0)
-//                    error_log_translate_warning(__FILE__."[".__LINE__."] Cols (".($this->col+1).") in line {$this->row} greater then maximum in one of previous lines ({$this->cols})!<br />");
+		function set_max()
+		{
+			if($this->col >= $this->cols)
+			{
+//				if($this->row > 0)
+//					error_log_translate_warning(__FILE__."[".__LINE__."] Cols (".($this->col+1).") in line {$this->row} greater then maximum in one of previous lines ({$this->cols})!<br />");
 
-	            $this->cols = $this->col + 1;
+				$this->cols = $this->col + 1;
 
-                if($this->rows == 0)
-                    $this->rows = 1;
-            }
+				if($this->rows == 0)
+					$this->rows = 1;
+			}
 
-            if($this->row >= $this->rows)
-                $this->rows = $this->row + 1;
-        }
+			if($this->row >= $this->rows)
+				$this->rows = $this->row + 1;
+		}
 
-        function next_col()
-        {
+		function next_col()
+		{
 			if(@$this->col_spans[$this->row][$this->col] > 1)
-	            $this->col += $this->col_spans[$this->row][$this->col];
+				$this->col += $this->col_spans[$this->row][$this->col];
 			else
-    	        $this->col++;
-        }
+				$this->col++;
+		}
 
-        function new_row()
-        {
-            $this->set_max();
-//            echo "{$this->row} {$this->col} {$this->cols}\n";
-//            if($this->row > 0 && $this->col != $this->cols-1)
-//              error_log_translate_warning(__FILE__."[".__LINE__."] Cols ({$this->col}) in line {$this->row} less then maximum in one of previous lines ({$this->cols})!<br />");
-            $this->col = 0;
-            $this->row++;
-        }
+		function new_row()
+		{
+			$this->set_max();
+//			echo "{$this->row} {$this->col} {$this->cols}\n";
+//			if($this->row > 0 && $this->col != $this->cols-1)
+//			  error_log_translate_warning(__FILE__."[".__LINE__."] Cols ({$this->col}) in line {$this->row} less then maximum in one of previous lines ({$this->cols})!<br />");
+			$this->col = 0;
+			$this->row++;
+		}
 
-        function setData($data)
-        {
-            $this->set_max();
-            $this->data[$this->row][$this->col] = $data;
-        }
+		function setData($data)
+		{
+			$this->set_max();
+			$this->data[$this->row][$this->col] = $data;
+		}
 
-        function append($data)
-        {
-            $this->setData($data);
-            $this->next_col();
-        }
+		function append($data)
+		{
+			$this->setData($data);
+			$this->next_col();
+		}
 
-        function setColSpan($col_span)
-        {
-            $this->col_spans[$this->row][$this->col] = $col_span;
-        }
+		function setColSpan($col_span)
+		{
+			$this->col_spans[$this->row][$this->col] = $col_span;
+		}
 
-        function setRowSpan($row_span)
-        {
-            $this->row_spans[$this->row][$this->col] = $row_span;
+		function setRowSpan($row_span)
+		{
+			$this->row_spans[$this->row][$this->col] = $row_span;
 			for($i=1; $i<$row_span; $i++)
 				$this->row_spans[$this->row+$i][$this->col] = -1;
-        }
+		}
 
-        function setHead($head_bit=1)
-        {
-            $this->heads[$this->row][$this->col] = $head_bit;
-        }
+		function setHead($head_bit=1)
+		{
+			$this->heads[$this->row][$this->col] = $head_bit;
+		}
 
-        function get_html()
-        {
-            $out = "<table{$this->table_width}{$this->table_border}>\n";
-            for($r=0; $r < $this->rows; $r++)
-            {
-				$out .= "<tr>";
+		var $style = array();
+		function addStyle($style) { $this->style[] = $style; }
+
+		function get_html()
+		{
+//			$table = Table:: "<table{$this->table_width}{$this->table_border}>\n";
+			$table = \HtmlObject\Element::table();
+			if($w = $this->table_width)
+			{
+				$table->width($w);
+				if(is_numeric($w))
+					$w = "{$w}px";
+				$this->addStyle("width:{$w}");
+			}
+
+			for($r=0; $r < $this->rows; $r++)
+			{
+				$tr = \HtmlObject\Element::tr();
+
 				$rspans = 0;
 				for($c=0; $c < $this->cols-1; $c++)
 					if(@$this->row_spans[$r][$c]<0)
 						$rspans++;
-                for($c=0; $c < $this->cols-1-$rspans; $c += @$this->col_spans[$r][$c] > 1 ? $this->col_spans[$r][$c] : 1)
+
+				for($c=0; $c < $this->cols-1-$rspans; $c += @$this->col_spans[$r][$c] > 1 ? $this->col_spans[$r][$c] : 1)
 				{
+
 //					echo "$r, $c: rss={$this->row_spans[$r][$c]} ";
 
 //	Убрано из-за	http://balancer.ru/2007/12/10/post-1361199.html
@@ -128,15 +143,32 @@
 					if($data == '')
 						$data = '&nbsp;';
 
-                    $tx = !empty($this->heads[$r][$c]) ? 'th' : 'td';
-					$colspan = @$this->col_spans[$r][$c] > 1 ? " colspan=\"".$this->col_spans[$r][$c]."\"" : "";
-					$rowspan = @$this->row_spans[$r][$c] > 1 ? " rowspan=\"".$this->row_spans[$r][$c]."\"" : "";
-                    $out .= "<{$tx}{$colspan}{$rowspan}>{$data}</{$tx}>";
-                }
-                $out .= "</tr>\n";
-//                echo "<br/>";
-            }
-            $out .= "</table>\n";
-            return $out;
-        }
-    }
+					$tx = !empty($this->heads[$r][$c]) ? \HtmlObject\Element::th() : \HtmlObject\Element::td();
+					if(@$this->col_spans[$r][$c] > 1)
+						$tx->colspan($this->col_spans[$r][$c]);
+					if(@$this->row_spans[$r][$c] > 1)
+						$tx->rowspan($this->row_spans[$r][$c]);
+
+					$tx->nest($data);
+
+					$tr->nest($tx);
+				}
+
+				$table->nest($tr);
+			}
+
+			if($this->layout)
+			{
+				if($class = $this->layout->get('table_class'))
+					$table->addClass($class);
+//				else
+//					$this->addstyle('border: 1px solid');
+			}
+
+			if($this->style)
+				$table->style(join('; ', $this->style));
+
+//			echo "<xmp>{$table}</xmp>";
+			return $table;
+		}
+}

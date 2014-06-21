dd7bfeee75f543141caabe79da9d6ed64c2f6df9
Balancer <balancer@balancer.ru>
2014-06-22 03:18:43 +0400

    Мелкие фиксы.

--- engines/bors/generated.php
+++ engines/bors/generated.php
@@ -1,9 +1,9 @@
 <?php
 
-if(function_exists('mb_strtolower') && config('internal_charset') == 'utf-8')
+if(function_exists('mb_strtolower') && strtolower(ini_get('default_charset')) == 'utf-8')
 {
 	// Фиг его знает, с чего PHP без этого перестал работать в консоли
-	ini_set('mbstring.internal_encoding', config('internal_charset'));
+	ini_set('mbstring.internal_encoding', 'UTF-8');
 	function bors_upper($str) { return mb_strtoupper($str); }
 	function bors_lower($str) { return mb_strtolower($str); }
 	function bors_strlen($str) { return mb_strlen($str); }

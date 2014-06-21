75f7483a2edfa34e4bfd9c1238fbba39ab73aae7
Balancer <balancer@balancer.ru>
2014-06-21 13:50:32 +0400

    Реализация работы с ядром без bors-инициализации.

--- classes/bors/object.php
+++ classes/bors/object.php
@@ -1,5 +1,7 @@
 <?php
 
+bors_funcs::init();
+
 // Идёт процесс рефакторинга с переносом функционала base_object в bors_object
 class bors_object extends base_object
 {
--- init.php
+++ init.php
@@ -372,21 +372,7 @@ function bors_auto_class($item_name, $action_name, $base_class_name, $attrs = ar
 	eval($code);
 }
 
-function mkpath($strPath, $mode=0777)
-{
-    if(!$strPath || is_dir($strPath) || $strPath=='/')
-        return true;
-
-	if(!($pStrPath = dirname($strPath)))
-		return true;
-
-	if(!mkpath($pStrPath, $mode)) 
-        return false;
-
-	$err = @mkdir($strPath, $mode);
-	@chmod($strPath, $mode);
-	return $err;
-}
+bors_funcs::init();
 
 function bors_include_once($file, $warn = false)
 {
@@ -417,83 +403,6 @@ function bors_include($file, $warn = false, $once = false)
 		return bors_throw($message);
 
 	echo $message;
-}
-
-/**
- * Извлекает поле $name из массива $data, если оно есть.
- * В противном случае возвращает $default.
- * @param array $data 
- * @param string $name
- * @param mixed $default
- * @return mixed
- */
-
-function defval($data, $name, $default=NULL)
-{
-	if($data && array_key_exists($name, $data))
-		return $data[$name];
-
-	return $default;
-}
-
-/**
- * Работает как и defval(), но при отсутствии
- * соответствующего элемента массива он создаётся в нём.
- * @param array $data
- * @param string $name
- * @param mixed $default
- * @return mixed
- */
-function defvalset(&$data, $name, $default=NULL)
-{
-	if($data && array_key_exists($name, $data))
-		return $data[$name];
-
-	return $data[$name] = $default;
-}
-
-/**
- * Аналогично defval(), но удаляет из массива данных извлечённое значение
- * @param array $data
- * @param string $name
- * @param mixed $default
- * @return mixed
- */
-function popval(&$data, $name, $default=NULL)
-{
-	if(!$data || !array_key_exists($name, $data))
-		return $default;
-
-	$ret = $data[$name];
-	unset($data[$name]);
-	return $ret;
-}
-
-/**
- * Аналогично defval(), но читается только непустое значение.
- * @param array $data
- * @param string $name
- * @param mixed $default
- * @return mixed
- */
-function defval_ne(&$data, $name, $default=NULL)
-{
-	if(!empty($data[$name]))
-		return $data[$name];
-
-	return $default;
-}
-
-/**
-	Устновить элемент массива $name в переменную $value, если он до этого не определён
-*/
-
-function set_def(&$data, $name, $value)
-{
-	if($data && array_key_exists($name, $data))
-		return $data[$name];
-
-	return $data[$name] = $value;
 }
 
 function nospace($str) { return str_replace(' ', '', $str); }

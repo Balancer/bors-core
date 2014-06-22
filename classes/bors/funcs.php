0b8ab96fc8146d4fac10dd06eb681ee82cfdd6c4
Balancer <balancer@balancer.ru>
2014-06-22 04:43:00 +0400

    Фиксы для запуска автономного и из Composer.

--- classes/bors/funcs.php
+++ classes/bors/funcs.php
@@ -22,7 +22,6 @@ class bors_funcs
 		}
 
 //		require BORS_CORE.'/classes/bors/class/loader.php';
-		require_once(__DIR__.'/../../engines/bors/generated.php');
 	}
 }
 
@@ -183,3 +182,31 @@ function config($key, $def = NULL) { return @array_key_exists($key, $GLOBALS['cm
 
 function config_seth($section, $hash, $key, $value) { return $GLOBALS['cms']['config'][$section][$hash][$key] = $value; }
 function configh($section, $hash, $key, $def = NULL) { return @array_key_exists($key, @$GLOBALS['cms']['config'][$section][$hash]) ? $GLOBALS['cms']['config'][$section][$hash][$key] : $def; }
+
+if(function_exists('mb_strtolower') && strtolower(ini_get('default_charset')) == 'utf-8')
+{
+	// Фиг его знает, с чего PHP без этого перестал работать в консоли
+	ini_set('mbstring.internal_encoding', 'UTF-8');
+	function bors_upper($str) { return mb_strtoupper($str); }
+	function bors_lower($str) { return mb_strtolower($str); }
+	function bors_strlen($str) { return mb_strlen($str); }
+	function bors_substr($str, $start, $length=NULL) { return is_null($length) ? mb_substr($str, $start) : mb_substr($str, $start, $length); }
+	function bors_strpos($str, $need, $start=NULL) { return is_null($start) ? mb_strpos($str, $need) : mb_strpos($str, $need, $start); }
+	function bors_strrpos($str, $need, $start=NULL) { return is_null($start) ? mb_strrpos($str, $need) : mb_strrpos($str, $need, $start); }
+	function bors_stripos($str, $need, $start=NULL) { return is_null($start) ? mb_stripos($str, $need) : mb_stripos($str, $need, $start); }
+	function bors_ucfirst($str) { return mb_substr(mb_strtoupper($str), 0, 1).mb_substr(mb_strtolower($str), 1); }
+}
+else
+{
+	function bors_lower($str) { return strtolower($str); }
+	function bors_upper($str) { return strtoupper($str); }
+	function bors_strlen($str) { return strlen($str); }
+	function bors_substr($str, $start, $length=NULL) { return is_null($length) ? substr($str, $start) : substr($str, $start, $length); }
+	function bors_strpos($str, $need, $start=NULL) { return is_null($start) ? strpos($str, $need) : strpos($str, $need, $start); }
+	function bors_strrpos($str, $need, $start=NULL) { return is_null($start) ? strrpos($str, $need) : strrpos($str, $need, $start); }
+	function bors_stripos($str, $need, $start=NULL) { return is_null($start) ? stripos($str, $need) : stripos($str, $need, $start); }
+	function bors_ucfirst($str) { return ucfirst($str); }
+}
+
+eval('class bors_log  extends '.config('log.class', 'bors_log_stub').' { } ');
+eval('class bors_time extends '.config('time.class', 'bors_time_ts').' { } ');

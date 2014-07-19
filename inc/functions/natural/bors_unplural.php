30326dcc7132b7a46af1467f2428d01461d421b7
Balancer <balancer@balancer.ru>
2014-07-20 03:43:29 +0400

    Перенос в blib_grammar::sungular()

--- inc/functions/natural/bors_unplural.php
+++ inc/functions/natural/bors_unplural.php
@@ -2,15 +2,5 @@
 
 function bors_unplural($s)
 {
-	if(preg_match('/rss$/', $s, $m)) // xrss -> xrss. Исключения не трогаем
-		return $s;
-	if(preg_match('/^(.+)ies$/', $s, $m)) // companies -> company
-		return $m[1].'y';
-	if(preg_match('/^(.+s)es$/', $s, $m)) // newses -> news
-		return $m[1];
-	if(preg_match('/^(.+)es$/', $s, $m)) // attaches -> attach
-		return $m[1];
-	if(preg_match('/^(.+)s$/', $s, $m)) // planes -> plane
-		return $m[1];
-	return $s;
+	return blib_grammar::singular($s);
 }

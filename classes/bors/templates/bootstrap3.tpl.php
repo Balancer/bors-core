e1e99e2418bd1a202e1e1e38bee16b802360d482
Balancer <balancer@balancer.ru>
2014-09-22 06:14:13 +0400

    Продолжаем разработку нового использования шаблонов.

--- classes/bors/templates/bootstrap3.tpl.php
+++ classes/bors/templates/bootstrap3.tpl.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="<?= htmlspecialchars($self->description()); ?>">
+	<meta name="description" content="<?= htmlspecialchars($self->description());/*"*/?>">
 	<meta name="author" content="">
 <?php /*	<link rel="icon" href="../../favicon.ico"> */?>
 
@@ -22,42 +22,59 @@
 	  <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	  <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-  </head>
+</head>
 
-  <body role="document">
+<body role="document">
 
 	<!-- Fixed navbar -->
 	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-	  <div class="container">
-		<div class="navbar-header">
-		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		  </button>
-		  <a class="navbar-brand" href="#">Bootstrap theme</a>
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
+				<a class="navbar-brand" href="<?= $self->project()->url();/*"*/?>"><?= htmlspecialchars($self->project()->title()); ?></a>
+			</div>
+<?php
+	if($nav_menu = $self->get('top_menu'))
+	{
+?>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+<?php
+		foreach($nav_menu as $title => $submenu)
+		{
+			if(is_array($submenu))
+			{
+?>
+				<li class="dropdown">
+<!--				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>-->
+<?php
+			bors_templates_bootstrap3_dropdown::show(array('menu' => array($title => $submenu))); ?>
+?>
+				</li>
+<?php
+			}
+			else
+			{
+				if(preg_match('/^\w+$/', $submenu))
+					$url = "/$submenu/";
+				else
+					$url = $submenu;
+				echo "<li class=\"active2\"><a href=\"{$url}\">".htmlspecialchars($title)."</a></li>\n";
+			}
+		}
+?>
+				</ul>
+			</div><!--/.nav-collapse -->
+<?php
+	}
+?>
 		</div>
-		<div class="navbar-collapse collapse">
-		  <ul class="nav navbar-nav">
-			<li class="active"><a href="#">Home</a></li>
-			<li><a href="#about">About</a></li>
-			<li><a href="#contact">Contact</a></li>
-			<li class="dropdown">
-			  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-			  <ul class="dropdown-menu" role="menu">
-				<li><a href="#">Action</a></li>
-				<li><a href="#">Another action</a></li>
-				<li><a href="#">Something else here</a></li>
-				<li class="divider"></li>
-				<li class="dropdown-header">Nav header</li>
-				<li><a href="#">Separated link</a></li>
-				<li><a href="#">One more separated link</a></li>
-			  </ul>
-			</li>
-		  </ul>
-		</div><!--/.nav-collapse -->
-	  </div>
 	</div>
 
 	<div class="container theme-showcase" role="main">

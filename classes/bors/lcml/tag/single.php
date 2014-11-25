f58c5f87c51ddd19ad21d3a5d9c3917a1b7b3211
Balancer <balancer@balancer.ru>
2014-11-25 17:36:41 +0300

    Доработка парсера LCML-разметки на тему автозагрузки классов парсеров.

--- classes/bors/lcml/tag/single.php
+++ classes/bors/lcml/tag/single.php
@@ -4,22 +4,23 @@ class bors_lcml_tag_single extends bors_lcml_tag
 {
 	function parse(&$params = array())
 	{
+		$tag_value = @$params[$params['tag_name']];
 		switch($this->lcml->output_type())
 		{
 			case 'text':
-				return $this->text(NULL, $params);
+				return $this->text($tag_value, $params);
 			case 'html_compact':
-				return $this->html_compact(NULL, $params);
+				return $this->html_compact($tag_value, $params);
 			case 'html_simple':
-				return $this->html_simple(NULL, $params);
+				return $this->html_simple($tag_value, $params);
 			case 'html':
 			default:
-				return $this->html(NULL, $params);
+				return $this->html($tag_value, $params);
 		}
 	}
 
 	function html($text, &$params) { return ''; }
-	function html_compact($text, &$params) { return $this->html(); }
-	function html_simple($text, &$params) { return $this->html(); }
-	function text($text, &$params) { return strip_tags($this->html()); }
+	function html_compact($text, &$params) { return $this->html($text, $params); }
+	function html_simple($text, &$params) { return $this->html($text, $params); }
+	function text($text, &$params) { return strip_tags($this->html($text, $params)); }
 }

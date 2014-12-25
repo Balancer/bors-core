10aecf4254b20b298325ffe288403b84f4ed8a29
Balancer <balancer@balancer.ru>
2014-12-25 05:27:09 +0300

    Упорядочены методы и подключён конфиг авто-файлов.

--- classes/bors/auto/search.php
+++ classes/bors/auto/search.php
@@ -1,40 +1,12 @@
 <?php
 
-/**
-	@nav_name = поиск
-*/
-
 class bors_auto_search extends bors_paginated
 {
-	function _title_def()
+	function action_url()
 	{
-		return ec('Поиск по ').call_user_func(array($this->main_class(), 'class_title_dpm'));
-	}
-
-	function _item_type_def()
-	{
-		$class = str_replace('/', '_', $this->args('class'));
-
-		$class = preg_replace('/^'.$this->project_name().'_/', '', $class);
-
-		if($class)
-			return blib_grammar::singular($class);
-
-		bors_throw(ec("Не задан тип искомых объектов и его не удаётся вычислить"));
-	}
-
-	function _project_name_def()
-	{
-		if($project = $this->args('project'))
-			return $project;
-
-		return config('project.name');
-	}
-
-	function _main_class_def()
-	{
-//		echo "project='{$this->project_name()}', item='{$this->item_type()}'<br/>";
-		return $this->project_name().'_'.$this->item_type();
+//		return bors()->request()->pure_url();
+		$path = '/'.str_replace('_', '/', bors_plural(str_replace('admin_', '', $this->item_type()))).'/search/';
+		return (strpos($this->args('class'), '_admin_') ? config('admin_site_url') : config('main_site_url')) . $path;
 	}
 
 	function body_data()
@@ -59,21 +31,48 @@ array
 		));
 	}
 
+	function _config_class_def() { return 'bors_auto_config'; }
+
+	function _item_type_def()
+	{
+		$class = str_replace('/', '_', $this->args('class'));
+
+		$class = preg_replace('/^'.$this->project_name().'_/', '', $class);
+
+		if($class)
+			return blib_grammar::singular($class);
+
+		bors_throw(ec("Не задан тип искомых объектов и его не удаётся вычислить"));
+	}
+
+	function _main_class_def()
+	{
+//		echo "project='{$this->project_name()}', item='{$this->item_type()}'<br/>";
+		return $this->project_name().'_'.$this->item_type();
+	}
+
+	var $nav_name_ec = 'поиск';
+
+	function _order_def() { return $this->args('order', 'title'); }
+
+	function page() { return bors()->request()->data('p', $this->default_page()); }
+
+	function _project_name_def()
+	{
+		if($project = $this->args('project'))
+			return $project;
+
+		return config('project.name');
+	}
+
 	function query()
 	{
 		return urldecode(bors()->request()->data('q'));
 	}
 
-	function where()
+	function _title_def()
 	{
-		return array('0');
-	}
-
-	function action_url()
-	{
-//		return bors()->request()->pure_url();
-		$path = '/'.str_replace('_', '/', bors_plural(str_replace('admin_', '', $this->item_type()))).'/search/';
-		return (strpos($this->args('class'), '_admin_') ? config('admin_site_url') : config('main_site_url')) . $path;
+		return ec('Поиск по ').call_user_func(array($this->main_class(), 'class_title_dpm'));
 	}
 
 	function url_ex($page)
@@ -89,10 +88,10 @@ array
 		return $url;
 	}
 
-	function page() { return bors()->request()->data('p', $this->default_page()); }
-
 	function url_skip_keys() { return 'q'; }
 
-	function _order_def() { return $this->args('order', 'title'); }
-//	function _order_def() { return $this->args('order', 'title'); }
+	function where()
+	{
+		return array('0');
+	}
 }

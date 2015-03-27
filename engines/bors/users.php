37e9e0ef1bc27122c09f9bd8aaec58edfa041190
Balancer <balancer@balancer.ru>
2015-03-27 19:28:08 +0300

    ++

--- engines/bors/users.php
+++ engines/bors/users.php
@@ -6,4 +6,9 @@ function bors_user($id)
 }
 
 if($user_class = config('user_class'))
+{
+	if(class_exists($user_class))
+		bors_throw("Unknown user class '$user_class'");
+
 	eval("class bors_user extends {$user_class} { function extends_class_name() { return '$user_class'; } }");
+}

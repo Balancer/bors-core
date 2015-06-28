4671865e052879eab8bd1d3ab237d5385c6a838c
Balancer <balancer@balancer.ru>
2015-06-28 07:10:13 +0300

    Laravel compatible.

--- classes/Cache.php
+++ classes/Cache.php
@@ -3,5 +3,5 @@
 require_once('engines/bors.php');
 
 $ce = config('cache_engine', 'bors_cache_base');
-eval('class Cache extends '.$ce.'{}');
+//eval('class Cache extends '.$ce.'{}');
 eval('class bors_cache extends '.$ce.'{}');

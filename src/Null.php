24936555041aa1c201f4ca30443708f47e7e49d3
Balancer <balancer@balancer.ru>
2015-10-22 18:19:48 +0300

    Null object and unit tests.

--- src/Null.php
+++ src/Null.php
@@ -9,5 +9,20 @@ class Null extends Object
 		return $this;
 	}
 
+	function __get($property)
+	{
+		return $this;
+	}
+
 	function __toString() { return ''; }
+
+	function isNull() { return true; }
+
+	static function __unit_test($suite)
+	{
+		$null = Null::factory();
+		$suite->assertNotNull($null->foo());
+		$suite->assertNotNull($null->foo);
+		$suite->assertTrue($null->isNull());
+	}
 }
--- src/Object.php
+++ src/Object.php
@@ -4,4 +4,11 @@ namespace B2;
 
 class Object extends \bors_object
 {
+	static function factory($id=NULL)
+	{
+		$called_class_name = get_called_class();
+		$object = new $called_class_name($id);
+		$object->_configure();
+		return $object;
+	}
 }

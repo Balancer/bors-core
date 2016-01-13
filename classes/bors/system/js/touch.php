691cd8e165a85a8e0730fc3fbef0846e421d49b1
Balancer <balancer@balancer.ru>
2016-01-14 00:48:20 +0300

    fix for empty json array.

--- classes/bors/system/js/touch.php
+++ classes/bors/system/js/touch.php
@@ -7,7 +7,7 @@ class bors_system_js_touch extends bors_js
 		$module_htmls = array();
 		if($modules = bors()->request()->data('modules'))
 		{
-			if(preg_match('/^\[\{/', $modules)) // JSON
+			if(preg_match('/^\[/', $modules)) // JSON
 			{
 				foreach(json_decode($modules, true) as $args)
 				{
@@ -15,12 +15,14 @@ class bors_system_js_touch extends bors_js
 						$module_htmls[] = array('id' => md5($x->internal_uri_ascii()), 'html' => $x->html());
 				}
 			}
-			else
+			elseif(is_array($modules))
 			{
 				foreach($modules as $uri)
 					if($x = bors_load_uri($uri))
 						$module_htmls[] = array('id' => md5($uri), 'html' => $x->html());
 			}
+			else
+				bors_debug::syslog('error-data', "Is not array: ".print_r($modules, true));
 		}
 
 		// А то на pre_show от модуля вылезает content-type от pors_page

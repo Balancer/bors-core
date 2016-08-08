6608107819033c4d10ffab9d7c8206357a97595d
Balancer <balancer@balancer.ru>
2016-08-08 17:47:11 +0300

    merge

--- tools/config.php
+++ tools/config.php
@@ -1,8 +1,13 @@
 <?php
 
-require_once __DIR__.'/../../../../setup.php';
+if(file_exists($f = __DIR__.'/../../../../setup.php'))
+	require_once $f;
 
 require_once BORS_CORE.'/init.php';
 config_set('system.session.skip', true);
 
-include_once(COMPOSER_ROOT.'/config-host.php');
+if(file_exists($f = COMPOSER_ROOT.'/config-host.php'))
+	require_once $f;
+
+if(file_exists($f = __DIR__.'/config-host.php'))
+	include_once($f);
